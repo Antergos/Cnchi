cfb6865a440f02bfffbd007e709390e67e491b23
Alexandre Filgueira <faidoc@gmail.com>
2013-03-16 20:05:24 +0000

    Fixed typo

--- src/installation_thread.py
+++ src/installation_thread.py
@@ -747,7 +747,7 @@ class InstallationThread(threading.Thread):
         ## Generate locales
         lang_code = self.settings.get("language_code")
         keyboard_variant = self.settings.get("keyboard_variant")
-        locale = '%s_%s' % (lang_code, timezone_zone)
+        locale = '%s_%s' % (lang_code, keyboard_variant)
         self.queue_event('info', _("Generating locales"))
         self.chroot(['sed', '-i', '-r', '"s/#(.*%s.*UTF-8)/\1/g"' % lang_code, "/etc/locale.gen"])
         self.chroot(['locale-gen'])
