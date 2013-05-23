6ad4412092e870a4534371d5b18cf71f2e6795a0
karasu <karasu@cinnarch.com>
2013-05-23 09:01:05 +0200

    There's an error in installation_automatic.ui that makes main window resize itself. Also in desktop.ui

--- src/parted/fs_module.py
+++ src/parted/fs_module.py
@@ -160,10 +160,10 @@ def resize(part, fs_type, new_size_in_mb):
 
 @misc.raise_privileges    
 def resize_ntfs(part, new_size_in_mb):
-    print("ntfsresize --size %sM %s" % (new_size_in_mb, part))
+    print("ntfsresize --size %sM %s" % (str(new_size_in_mb), part))
 
     try:
-        x = subprocess.check_output(["ntfsresize", "--size", new_size_in_mb+"M", part])
+        x = subprocess.check_output(["ntfsresize", "--size", str(new_size_in_mb)+"M", part])
     except Exception as e:
         x = None
         print(e)
@@ -179,10 +179,10 @@ def resize_fat(part, new_size_in_mb):
     
 @misc.raise_privileges
 def resize_ext(part, new_size_in_mb):
-    print("resize2fs %s %sM" % (part, new_size_in_mb))
+    print("resize2fs %s %sM" % (part, str(new_size_in_mb)))
 
     try:
-        x = subprocess.check_output(["resize2fs", part, new_size_in_mb+"M"])
+        x = subprocess.check_output(["resize2fs", part, str(new_size_in_mb)+"M"])
     except Exception as e:
         x = None
         print(e)
