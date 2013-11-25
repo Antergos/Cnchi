cee878d152339cba9a54c9a37fd5b5d5b998add8
karasu <karasu@cinnarch.com>
2013-11-25 18:04:00 +0100

    Beautified bootinfo.py

--- src/bootinfo.py
+++ src/bootinfo.py
@@ -20,112 +20,92 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 
+""" Detects installed OSes """
+
 import os
 import subprocess
 import re
-import logging
+
+# constants
+WIN_DIRS = ["windows", "Windows", "WINDOWS"]
+SYSTEM_DIRS = ["System32", "system32"]
+
+WINLOAD_NAMES = ["Winload.exe", "winload.exe"]
+SECEVENT_NAMES = ["SecEvent.Evt", "secevent.evt"]
+DOS_NAMES = ["IO.SYS", "io.sys"]
+LINUX_NAMES = ["issue", "slackware_version"]
+
+VISTA_MARK = "W.i.n.d.o.w.s. .V.i.s.t.a"
+SEVEN_MARK = "W.i.n.d.o.w.s. .7"
+DOS_MARKS = ["MS-DOS", "MS-DOS 6.22", "MS-DOS 6.21", "MS-DOS 6.0", \
+             "MS-DOS 5.0", "MS-DOS 4.01", "MS-DOS 3.3", "Windows 98" \
+             "Windows 95"]
 
 if __name__ == '__main__':
     import gettext
     _ = gettext.gettext
 
 def get_os(mountname):
+    """ Detect installed OSes """
     #  If partition is mounted, try to identify the Operating System
     # (OS) by looking for files specific to the OS.
-    OS = _("unknown")
 
-    win_dirs = ["windows", "Windows", "WINDOWS"]
-    system_dirs = ["System32", "system32"]
-    winload_names = ["Winload.exe", "winload.exe"]
-    secevent_names = ["SecEvent.Evt", "secevent.evt"]
+    detected_os = _("unknown")
 
-    vista_mark = "W.i.n.d.o.w.s. .V.i.s.t.a"
-    seven_mark = "W.i.n.d.o.w.s. .7"
+    for windows in WIN_DIRS:
+        for system in SYSTEM_DIRS:
+            for name in WINLOAD_NAMES:
+                path = os.path.join(mountname, windows, system, name)
+                if os.path.exists(path):
+                    with open(path, "rb") as system_file:
+                        if VISTA_MARK in system_file:
+                            detected_os = "Windows Vista"
+                        elif SEVEN_MARK in system_file:
+                            detected_os = "Windows 7"
+            if detected_os == _("unknown"):
+                for name in SECEVENT_NAMES:
+                    path = os.path.join(mountname, windows, system, "config", name)
+                    if os.path.exists(path):
+                        detected_os = "Windows XP"
 
-    for windows in win_dirs:
-        for system in system_dirs:
-            for name in winload_names:
-                p = os.path.join(mountname, windows, system, name)
-                if os.path.exists(p):
-                    with open(p, "rb") as f:
-                        if vista_mark in f:
-                            OS = "Windows Vista"
-                        elif seven_mark in f:
-                            OS = "Windows 7"
-            if OS == _("unknown"):
-                for name in secevent_names:
-                    p = os.path.join(mountname, windows, system, "config", name)
-                    if os.path.exists(p):
-                        OS = "Windows XP"
-    if OS == _("unknown"):
-        p = os.path.join(mountname, "ReactOS/system32/config/SecEvent.Evt")
-        if os.path.exists(p):
-            OS = "ReactOS"
+    if detected_os == _("unknown"):
+        path = os.path.join(mountname, "ReactOS/system32/config/SecEvent.Evt")
+        if os.path.exists(path):
+            detected_os = "ReactOS"
 
-    dos_marks = ["MS-DOS", "MS-DOS 6.22", "MS-DOS 6.21", "MS-DOS 6.0", \
-                 "MS-DOS 5.0", "MS-DOS 4.01", "MS-DOS 3.3", "Windows 98" \
-                 "Windows 95"]
-
-    dos_names = ["IO.SYS", "io.sys"]
-
-    for name in dos_names:
-        p = os.path.join(mountname, name)
-        if os.path.exists(p):
-            with open(p, "rb") as f:
-                for mark in dos_marks:
-                    if mark in f:
-                        OS = mark
-
+    for name in DOS_NAMES:
+        path = os.path.join(mountname, name)
+        if os.path.exists(path):
+            with open(path, "rb") as system_file:
+                for mark in DOS_MARKS:
+                    if mark in system_file:
+                        detected_os = mark
     # Linuxes
 
-    if OS == _("unknown"):
-        linux_names = ["issue", "slackware_version"]
-        for name in linux_names:
-            p = os.path.join(mountname, "etc", name)
-            if os.path.exists(p):
-                with open(p, "rt") as f:
-                    t = f.readline()
-                textlist = t.split()
+    if detected_os == _("unknown"):
+        for name in LINUX_NAMES:
+            path = os.path.join(mountname, "etc", name)
+            if os.path.exists(path):
+                with open(path, 'r') as system_file:
+                    line = system_file.readline()
+                textlist = line.split()
                 text = ""
-                for l in textlist:
-                    if not "\\" in l:
-                        text = text + l
-                OS = text
+                for element in textlist:
+                    if not "\\" in element:
+                        text = text + element
+                detected_os = text
 
-    return OS
-
-
-def get_devices_and_their_mount_points():
-    d = {}
-    try:
-        out = subprocess.check_output(["mount"]).decode()
-    except (subprocess.CalledProcessError, FileNotFoundError) as e:
-        logging.exception(e)
-        return d
-
-    out = out.split("\n")
-
-    for line in out:
-        e = line.split()
-        try:
-            device = e[0]
-            if "sd" in device and re.search(r'\d+$', device):
-                # ok, it has sd and ends with a number
-                d[device] = e[2]
-        except:
-            pass
-
-    return d
-
+    return detected_os
 
 def get_os_dict():
+    """ Returns all detected OSes in a dict """
     oses = {}
 
-    with open("/proc/partitions", "rt") as f:
-        for line in f:
-            l = line.split()
-            if len(l) > 0:
-                device = l[3]
+    with open("/proc/partitions", 'r') as partitions_file:
+        for line in partitions_file:
+            line_split = line.split()
+            if len(line_split) > 0:
+                device = line_split[3]
                 if "sd" in device and re.search(r'\d+$', device):
                     # ok, it has sd and ends with a number
                     device = "/dev/" + device
@@ -141,6 +121,4 @@ def get_os_dict():
     return oses
 
 if __name__ == '__main__':
-    oses = {}
-    oses = get_os_dict()
-    print (oses)
+    print(get_os_dict())
