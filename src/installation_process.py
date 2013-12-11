Antergos/Cnchi

commit 095693feaeafe516dc02e64e5cc8fcf9299161b8

Some cleanup in installation_process.py

src/installation_process.py
@@ -1166,16 +1166,80 @@ class InstallationProcess(multiprocessing.Process):
             if os.path.exists(service):
                 self.enable_services(['ufw'])
 
+    def set_autologin(self):
+        """ Enables automatic login for the installed desktop manager """
+        self.queue_event('info', _("%s: Enable automatic login for user %s.") % (self.desktop_manager, username))
+
+        if self.desktop_manager == 'gdm':
+            # Systems with GDM as Desktop Manager
+            gdm_conf_path = os.path.join(self.dest_dir, "etc/gdm/custom.conf")
+            with open(gdm_conf_path, "w") as gdm_conf:
+                gdm_conf.write('# Cnchi - Enable automatic login for user\n')
+                gdm_conf.write('[daemon]\n')
+                gdm_conf.write('AutomaticLogin=%s\n' % username)
+                gdm_conf.write('AutomaticLoginEnable=True\n')
+        elif self.desktop_manager == 'kdm':
+            # Systems with KDM as Desktop Manager
+            kdm_conf_path = os.path.join(self.dest_dir, "usr/share/config/kdm/kdmrc")
+            text = []
+            with open(kdm_conf_path, "r") as kdm_conf:
+                text = kdm_conf.readlines()
+            with open(kdm_conf_path, "w") as kdm_conf:
+                for line in text:
+                    if '#AutoLoginEnable=true' in line:
+                        line = 'AutoLoginEnable=true\n'
+                    if 'AutoLoginUser=' in line:
+                        line = 'AutoLoginUser=%s\n' % username
+                    kdm_conf.write(line)
+        elif self.desktop_manager == 'lxdm':
+            # Systems with LXDM as Desktop Manager
+            lxdm_conf_path = os.path.join(self.dest_dir, "etc/lxdm/lxdm.conf")
+            text = []
+            with open(lxdm_conf_path, "r") as lxdm_conf:
+                text = lxdm_conf.readlines()
+            with open(lxdm_conf_path, "w") as lxdm_conf:
+                for line in text:
+                    if '# autologin=dgod' in line:
+                        line = 'autologin=%s\n' % username
+                    lxdm_conf.write(line)
+        elif self.desktop_manager == 'lightdm':
+            # Systems with LightDM as Desktop Manager
+            # Ideally, we should use configparser for the ini conf file,
+            # but we just do a simple text replacement for now, as it worksforme(tm)
+            lightdm_conf_path = os.path.join(self.dest_dir, "etc/lightdm/lightdm.conf")
+            text = []
+            with open(lightdm_conf_path, "r") as lightdm_conf:
+                text = lightdm_conf.readlines()
+            with open(lightdm_conf_path, "w") as lightdm_conf:
+                for line in text:
+                    if '#autologin-user=' in line:
+                        line = 'autologin-user=%s\n' % username
+                    lightdm_conf.write(line)
+        elif self.desktop_manager == 'slim':
+            # Systems with Slim as Desktop Manager
+            slim_conf_path = os.path.join(self.dest_dir, "etc/slim.conf")
+            text = []
+            with open(slim_conf_path, "r") as slim_conf:
+                text = slim_conf.readlines()
+            with open(slim_conf_path, "w") as slim_conf:
+                for line in text:
+                    if 'auto_login' in line:
+                        line = 'auto_login yes\n'
+                    if 'default_user' in line:
+                        line = 'default_user %s\n' % username
+                    slim_conf.write(line)
+
     def configure_system(self):
-        """ Final install steps """        
-        # Hide progress bars
-        self.queue_event('progress', 'hide_all')
+        """ Final install steps
+            Set clock, language, timezone
+            Run mkinitcpio
+            Populate pacman keyring
+            Setup systemd services
+            ... and more """
         
-        # set clock, language, timezone
-        # run mkinitcpio
-        # populate pacman keyring
-        # setup systemd services
-        # ... check configure_system from arch-setup
+        # All downloading and installing has been done, so we hide progress bars
+        # (they are 100% both so it makes no sense to still show them)
+        self.queue_event('progress', 'hide_all')
 
         self.queue_event('action', _("Configuring your new system"))
 
@@ -1209,7 +1273,9 @@ class InstallationProcess(multiprocessing.Process):
         try:
             shutil.copy2('/etc/pacman.d/mirrorlist', mirrorlist_path)
             self.queue_event('debug', _('Mirror list copied.'))
-        except:
+        except FileNotFoundError:
+            logging.warning(_("Can't copy mirrorlist file"))
+        except FileExistsError:
             pass
 
         # Copy important config files to target system
@@ -1218,8 +1284,9 @@ class InstallationProcess(multiprocessing.Process):
         for path in files:
             try:
                 shutil.copy2(path, os.path.join(self.dest_dir, 'etc/'))
-            except:
-                pass
+            except FileNotFoundError, FileExistsError:
+                logging.warning(_("Can't copy %s file") % path)
+
         self.queue_event('debug', _('Important configuration files copied.'))
 
         desktop = self.settings.get('desktop')
@@ -1283,7 +1350,7 @@ class InstallationProcess(multiprocessing.Process):
             with open(hostname_path, "w") as hostname_file:
                 hostname_file.write(hostname)
 
-        self.queue_event('debug', _('Hostname  %s set.') % hostname)
+        self.queue_event('debug', _('Hostname set to %s.') % hostname)
 
         # User password is the root password
         self.change_user_password('root', password)
@@ -1327,64 +1394,6 @@ class InstallationProcess(multiprocessing.Process):
                 xorg_conf_xkb.write('EndSection\n')
             self.queue_event('debug', _("00-keyboard.conf written."))
 
-            # Set autologin if selected
-            if self.settings.get('require_password') is False:
-                self.queue_event('info', _("%s: Enable automatic login for user %s.") % (self.desktop_manager, username))
-                # Systems with GDM as Desktop Manager
-                if self.desktop_manager == 'gdm':
-                    gdm_conf_path = os.path.join(self.dest_dir, "etc/gdm/custom.conf")
-                    with open(gdm_conf_path, "wt") as gdm_conf:
-                        gdm_conf.write('# Enable automatic login for user\n')
-                        gdm_conf.write('[daemon]\n')
-                        gdm_conf.write('AutomaticLogin=%s\n' % username)
-                        gdm_conf.write('AutomaticLoginEnable=True\n')
-
-                # Systems with KDM as Desktop Manager
-                elif self.desktop_manager == 'kdm':
-                    kdm_conf_path = os.path.join(self.dest_dir, "usr/share/config/kdm/kdmrc")
-                    text = []
-                    with open(kdm_conf_path, "r") as kdm_conf:
-                        text = kdm_conf.readlines()
-
-                    with open(kdm_conf_path, "w") as kdm_conf:
-                        for line in text:
-                            if '#AutoLoginEnable=true' in line:
-                                line = '#AutoLoginEnable=true \n'
-                                line = line[1:]
-                            if 'AutoLoginUser=' in line:
-                                line = 'AutoLoginUser=%s \n' % username
-                            kdm_conf.write(line)
-
-                # Systems with LXDM as Desktop Manager
-                elif self.desktop_manager == 'lxdm':
-                    lxdm_conf_path = os.path.join(self.dest_dir, "etc/lxdm/lxdm.conf")
-                    text = []
-                    with open(lxdm_conf_path, "r") as lxdm_conf:
-                        text = lxdm_conf.readlines()
-
-                    with open(lxdm_conf_path, "w") as lxdm_conf:
-                        for line in text:
-                            if '# autologin=dgod' in line and line[0] == "#":
-                                # uncomment line
-                                line = '# autologin=%s' % username
-                                line = line[1:]
-                            lxdm_conf.write(line)
-
-                # Systems with LightDM as the Desktop Manager
-                elif self.desktop_manager == 'lightdm':
-                    lightdm_conf_path = os.path.join(self.dest_dir, "etc/lightdm/lightdm.conf")
-                    # Ideally, use configparser for the ini conf file, but just do
-                    # a simple text replacement for now
-                    text = []
-                    with open(lightdm_conf_path, "r") as lightdm_conf:
-                        text = lightdm_conf.readlines()
-
-                    with open(lightdm_conf_path, "w") as lightdm_conf:
-                        for line in text:
-                            if '#autologin-user=' in line:
-                                line = 'autologin-user=%s\n' % username
-                            lightdm_conf.write(line)
-
         # Let's start without using hwdetect for mkinitcpio.conf.
         # I think it should work out of the box most of the time.
         # This way we don't have to fix deprecated hooks.
@@ -1392,35 +1401,33 @@ class InstallationProcess(multiprocessing.Process):
         self.queue_event('info', _("Running mkinitcpio..."))
         self.run_mkinitcpio()
 
-        self.queue_event('debug', _("Call post-install script to execute gsettings commands"))
+        self.queue_event('debug', _("Call Cnchi post-install script"))
         # Call post-install script to execute gsettings commands
         script_path_postinstall = os.path.join(self.settings.get('cnchi'), "scripts", POSTINSTALL_SCRIPT)
         subprocess.check_call(["/usr/bin/bash", script_path_postinstall, \
             username, self.dest_dir, self.desktop, keyboard_layout, keyboard_variant])
 
-        # In openbox "desktop", the postinstall script writes /etc/slim.conf
-        # so we have to modify it here (after running the script).
         # Set autologin if selected
-        if self.settings.get('require_password') is False and \
-           self.desktop_manager == 'slim':
-            slim_conf_path = os.path.join(self.dest_dir, "etc/slim.conf")
-            text = []
-            with open(slim_conf_path, "r") as slim_conf:
-                text = slim_conf.readlines()
-            with open(slim_conf_path, "w") as slim_conf:
-                for line in text:
-                    if 'auto_login' in line:
-                        line = 'auto_login yes\n'
-                    if 'default_user' in line:
-                        line = 'default_user %s\n' % username
-                    slim_conf.write(line)
+        # Warning: In openbox "desktop", the postinstall script writes /etc/slim.conf
+        # so we have to modify it here (after running the post-install script).
+        if self.settings.get('require_password') is False:
+            self.set_autologin()
 
         # Configure user features
         self.setup_features()
 
+        # Encrypt user's home directory if requested
         if self.settings.get('encrypt_home'):
-            # Encrypt user's home directory if requested
             self.queue_event('debug', _("Encrypting user home dir..."))
             encfs.setup(username, self.dest_dir)
             self.queue_event('debug', _("User home dir encrypted"))
 
+        # Last but not least, copy Cnchi log to new installation
+        datetime = time.strftime("%Y%m%d") + "-" + time.strftime("%H%M%S")
+        dst = os.path.join(self.dest_dir, "var/log/cnchi-%s.log" % datetime)
+        try:
+            shutil.copy("/tmp/cnchi.log", dst)
+        except FileNotFoundError:
+            logging.warning(_("Can't copy Cnchi log to /var/log"))
+        except FileExistsError:
+            pass

src/pacman/action_sync.py
@@ -1,264 +1,0 @@
-#!/usr/bin/env python3
-#
-# pycman-sync - A Python implementation of Pacman
-# Copyright (C) 2011 Rémy Oudompheng <remy@archlinux.org>
-#
-#   This program is free software; you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation; either version 2 of the License, or
-#   (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program; if not, write to the Free Software
-#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-
-"""
-A Python implementation of pacman -S
-
-This script displays information about packages available in repositories,
-and is also used to install/upgrade/remove them.
-"""
-
-import sys
-
-import pyalpm
-from pycman import config
-from pycman import pkginfo
-from pycman import transaction
-
-handle = None
-
-def do_clean(options):
-	raise NotImplementedError
-
-def do_refresh(options):
-	"Sync databases like pacman -Sy"
-	force = (options.refresh > 1)
-	for db in handle.get_syncdbs():
-		t = transaction.init_from_options(handle, options)
-		db.update(force)
-		t.release()
-	return 0
-
-def do_sysupgrade(options):
-	"Upgrade a system like pacman -Su"
-	downgrade = (options.sysupgrade > 1)
-	t = transaction.init_from_options(handle, options)
-	t.sysupgrade(downgrade)
-	if len(t.to_add) + len(t.to_remove) == 0:
-		print("nothing to do")
-		t.release()
-		return 0
-	else:
-		ok = transaction.finalize(t)
-		return (0 if ok else 1)
-
-def do_install(pkgs, options):
-	"Install a list of packages like pacman -S"
-	repos = dict((db.name,db) for db in handle.get_syncdbs())
-	if len(pkgs) == 0:
-		print("error: no targets specified")
-		return 1
-
-	targets = []
-	for name in pkgs:
-		ok, pkg = find_sync_package(name, repos)
-		if not ok:
-			print('error:', pkg)
-			return 1
-		else:
-			targets.append(pkg)
-	t = transaction.init_from_options(handle, options)
-	[t.add_pkg(pkg) for pkg in targets]
-	ok = transaction.finalize(t)
-	return (0 if ok else 1)
-
-def find_sync_package(pkgname, syncdbs):
-	"Finds a package name of the form 'repo/pkgname' or 'pkgname' in a list of DBs"
-	if '/' in pkgname:
-		repo, pkgname = pkgname.split('/', 1)
-		db = syncdbs.get(repo)
-		if db is None:
-			return False, "repository '%s' does not exist" % repo
-		pkg = db.get_pkg(pkgname)
-		if pkg is None:
-			return False, "package '%s' was not found in repository '%s'" % (pkgname, repo)
-		return True, pkg
-	else:
-		for db in syncdbs.values():
-			pkg = db.get_pkg(pkgname)
-			if pkg is not None:
-				return True, pkg
-		return False, "package '%s' was not found" % pkgname
-
-# Query actions
-
-def show_groups(args):
-	"Show groups like pacman -Sg"
-	for repo in handle.get_syncdbs():
-		if len(args.args) == 0:
-			# list all available groups
-			[print(name) for name, pkgs in repo.grpcache]
-		else:
-			# only print chosen groups
-			for group in args.args:
-				grp = repo.read_grp(group)
-				if grp is None:
-					continue
-				else:
-					name, pkgs = grp
-				if args.quiet:
-					[print(pkg.name) for pkg in pkgs]
-				else:
-					[print(name, pkg.name) for pkg in pkgs]
-	return 0
-
-def show_repo(args):
-	"Show repository's list of packages like pacman -Sl"
-	repos = handle.get_syncdbs()
-	if len(args.args) > 0:
-		repo_dict = dict((repo.name, repo) for repo in repos)
-		try:
-			repos = [repo_dict[name] for name in args.args]
-		except KeyError as err:
-			print("error: repository '%s' was not found" % err.args)
-			return 1
-
-	for repo in repos:
-		if args.quiet:
-			[print(pkg.name) for pkg in repo.pkgcache]
-		else:
-			[print(repo.name, pkg.name, pkg.version) for pkg in repo.pkgcache]
-	return 0
-
-def show_packages(args):
-	"Show information about packages like pacman -Si"
-	retcode = 0
-	if len(args.args) == 0:
-		for repo in handle.get_syncdbs():
-			for pkg in repo.pkgcache:
-				pkginfo.display_pkginfo(pkg, level = args.info, style = 'sync')
-	else:
-		repos = dict((db.name,db) for db in handle.get_syncdbs())
-		for pkgname in args.args:
-			ok, value = find_sync_package(pkgname, repos)
-			if ok:
-				pkginfo.display_pkginfo(value, level = args.info, style = 'sync')
-			else:
-				retcode = 1
-				print("error:", value)
-	return retcode
-
-def show_search(patterns, options):
-	results = []
-	for db in handle.get_syncdbs():
-		results += db.search(*patterns)
-	if len(results) == 0:
-		return 1
-	for pkg in results:
-		if options.quiet:
-			print(pkg.name)
-		else:
-			print("%s/%s %s" % (pkg.db.name, pkg.name, pkg.version))
-			print("    " + pkg.desc)
-	return 0
-
-def parse_options(rawargs):
-	parser = config.make_parser(prog = 'pycman-sync')
-	# Misc actions
-	group0 = parser.add_argument_group("Actions (default is installing specified packages)")
-	group0.add_argument("-c", "--clean",
-			action = 'count', default = 0,
-			help = 'remove old packages from cache directory (-cc for all)')
-	group0.add_argument("-u", "--sysupgrade",
-			action = 'count', default = 0,
-			help = 'upgrade installed packages (-uu allows downgrade)')
-	group0.add_argument("-y", "--refresh",
-			action = 'count', default = 0,
-			help = 'download fresh package databases from the server')
-	# Installation options
-	grp_install = parser.add_argument_group("Install options")
-	grp_install.add_argument('-d', '--nodeps',
-			action = 'store_true', default = False,
-			help = 'skip dependency checks')
-	grp_install.add_argument('-f', '--force',
-			action = 'store_true', default = False,
-			help = 'force install, overwrite conflicting files')
-	grp_install.add_argument('-k', '--dbonly',
-			action = 'store_true', default = False,
-			help = 'only modify database entries, not package files')
-	grp_install.add_argument('-w', '--downloadonly',
-			action = 'store_true', default = False,
-			help = 'download packages but do not install/upgrade anything')
-	grp_install.add_argument('--asdeps', dest = 'mode',
-			action = "store_const",
-			const = pyalpm.PKG_REASON_DEPEND)
-	grp_install.add_argument('--asexplicit', dest = 'mode',
-			action = "store_const",
-			const = pyalpm.PKG_REASON_EXPLICIT)
-	# Options to query sync databases
-	group1 = parser.add_argument_group("Query actions")
-	group1.add_argument('-g', '--groups', action = 'store_true', default = False,
-			help = 'view list of groups, or all members of a package group')
-	group1.add_argument('-i', '--info',
-			action = 'count', dest = 'info', default = 0,
-			help = 'view package information')
-	group1.add_argument('-l', '--list', action = 'store_true', default = False,
-			help = 'list the contents of repositories')
-	group1.add_argument('-s', '--search', action = 'store_true', default = False,
-			help = 'search remote repositories for matching strings')
-	group1.add_argument('-q', '--quiet',
-			action = 'store_true', dest = 'quiet', default = False,
-			help = 'show less information for query and search')
-	group1.add_argument('args', metavar = 'arg', nargs = '*',
-			help = 'arguments (group names for -g, repo names for -l, '
-			'package names for -i)')
-	return parser.parse_args(rawargs)
-
-def main(rawargs):
-	global handle
-	args = parse_options(rawargs)
-	handle = config.init_with_config_and_options(args)
-
-	if args.verbose:
-		print("sync " + " ".join(rawargs), file = sys.stderr)
-
-	# Refresh databases if necessary
-	if args.refresh > 0:
-		ret = do_refresh(args)
-		if ret != 0:
-			return ret
-
-	# If a query action is set
-	if args.search:
-		return show_search(args.args, args)
-	elif args.groups:
-		return show_groups(args)
-	elif args.info:
-		return show_packages(args)
-	elif args.list:
-		return show_repo(args)
-	# If a cleanup is required
-	elif args.clean > 0:
-		return do_clean(args)
-	# If a sysupgrade is required
-	elif args.sysupgrade > 0:
-		return do_sysupgrade(args)
-	# If only a refresh was requested
-	elif len(args.args) == 0 and args.refresh > 0:
-		return 0
-	# Otherwise it's a normal install
-	else:
-		return do_install(args.args, args)
-
-if __name__ == "__main__":
-	ret = main(sys.argv[1:])
-	sys.exit(ret)
-
-# vim: set ts=4 sw=4 tw=0 noet:

src/pacman/config.py
@@ -33,7 +33,10 @@ import argparse
 import collections
 import warnings
 
-import pyalpm
+try:
+    import pyalpm
+except ImportError:
+    pass
 
 class InvalidSyntax(Warning):
     """ Class to show warning when a pacman.conf parse error is issued """

src/pacman/pac.py
@@ -32,11 +32,11 @@ import queue
 
 try:
     import pyalpm
-    #from pacman import config
     import pacman.config
-except:
-    print("pyalpm not found! This installer won't work.")
-    sys.exit(1)
+except ImportError:
+    msg = _("pyalpm not found! This installer won't work.")
+    print(msg)
+    logging.error(msg)
 
 class Pac(object):
     """ Comunicates with libalpm using pyalpm """

commit 03766a6dcc4f41f54136645d5cb74b9f234bafca

Fixed bug introduced in commit https://github.com/Antergos/Cnchi/commit/095693feaeafe516dc02e64e5cc8fcf9299161b8#commitcomment-4831428 Thanks to Phil (@manjaro)

src/installation_process.py
@@ -1165,6 +1165,7 @@ class InstallationProcess(multiprocessing.Process):
 
     def set_autologin(self):
         """ Enables automatic login for the installed desktop manager """
+        username = self.settings.get('username')
         self.queue_event('info', _("%s: Enable automatic login for user %s.") % (self.desktop_manager, username))
 
         if self.desktop_manager == 'gdm':