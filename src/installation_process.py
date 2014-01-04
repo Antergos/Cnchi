814c88c5fbe75cd127559c2ec0d2349bd875da21
Dustin Falgout <dustin@falgout.us>
2014-01-03 18:38:43 -0600

    corrected typo in LUKS mkinitcpio config

--- src/installation_process.py
+++ src/installation_process.py
@@ -1169,7 +1169,7 @@ class InstallationProcess(multiprocessing.Process):
 
         if self.settings.get("use_luks"):
             hooks.append("encrypt")
-            modules.extend([ "dm_mod", "dm_crypt", "ext4", "aes-x86_64", "sha256", "sha512" ])
+            modules.extend([ "dm_mod", "dm_crypt", "ext4", "aes_x86_64", "sha256", "sha512" ])
 
         if self.blvm or self.settings.get("use_lvm"):
             hooks.append("lvm2")
