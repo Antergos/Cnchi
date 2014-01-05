9eb0303cb2d93813f851aa4113e93a931fd4f17e
Dustin Falgout <dustin@falgout.us>
2014-01-05 11:02:14 -0600

    made progress on MATE install. still needs work. we probably don't want to install entire mate-extra group and instead pick the individual packages ourselves.

--- src/installation_process.py
+++ src/installation_process.py
@@ -388,7 +388,7 @@ class InstallationProcess(multiprocessing.Process):
             tmp_file.write("Server = http://antergos.info/repo/testing\n\n")
 
             # Until MATE makes it into the official repos..
-            if self.desktop is 'mate':
+            if self.desktop == 'mate':
                 tmp_file.write("[mate]\n")
                 tmp_file.write("SigLevel = Optional TrustAll\n")
                 tmp_file.write("Server = http://repo.mate-desktop.org/archlinux/$arch\n\n")
