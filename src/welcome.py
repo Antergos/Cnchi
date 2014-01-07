7177546b5c6e263fde5e80eb8a9a1b7fc6240d93
karasu <gustaucastells@gmail.com>
2014-01-07 23:11:07 +0100

    Fixed welcome screen (I modified it before to test keymap and forgot to change again)

--- src/welcome.py
+++ src/welcome.py
@@ -31,8 +31,7 @@ from show_message import warning
 # Import functions
 import config
 
-#_next_page = "language"
-_next_page = "keymap"
+_next_page = "language"
 _prev_page = None
 
 class Welcome(Gtk.Box):
