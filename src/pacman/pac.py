82b12898bdaf1523b91634355a44b12893f21715
karasu <gustaucastells@gmail.com>
2014-01-14 10:54:53 +0100

    Modified comments and removed ugly .db and .xz extensions from download messages

--- src/pacman/pac.py
+++ src/pacman/pac.py
@@ -69,9 +69,6 @@ class Pac(object):
             self.handle.progresscb = self.cb_progress
             self.handle.logcb = self.cb_log
 
-    ###################################################################
-    # Transaction
-
     def finalize(self, t):
         """ Commit a transaction """
         try:
@@ -116,9 +113,6 @@ class Pac(object):
                 allexplicit = (getattr(options, 'mode', None) == pyalpm.PKG_REASON_EXPLICIT))
         return t
     '''
-
-    ###################################################################
-    # pacman -Sy (refresh) and pacman -S (install)
 
     def do_refresh(self):
         """ Sync databases like pacman -Sy """
@@ -180,7 +174,7 @@ class Pac(object):
         return (0 if ok else 1)
 
     def find_sync_package(self, pkgname, syncdbs):
-        # Finds a package name in a list of DBs
+        """ Finds a package name in a list of DBs """
         for db in syncdbs.values():
             pkg = db.get_pkg(pkgname)
             if pkg is not None:
@@ -188,7 +182,7 @@ class Pac(object):
         return False, "Package '%s' was not found." % pkgname
 
     def get_group_pkgs(self, group):
-        # Get group packages
+        """ Get group packages """
         for repo in self.handle.get_syncdbs():
             grp = repo.read_grp(group)
             if grp is None:
@@ -198,20 +192,17 @@ class Pac(object):
                 return pkgs
         return None
 
-    ###################################################################
-    # Queue event
-
     def queue_event(self, event_type, event_text=""):
+        """ Queues events to the event list in the GUI thread """
         if event_type in self.last_event:
             if self.last_event[event_type] == event_text:
-                # do not repeat same event
+                # Do not enqueue the same event twice
                 return
 
         self.last_event[event_type] = event_text
 
         if event_type == "error":
-            # format message to show file, function, and line where the error
-            # was issued
+            # Format message to show file, function, and line where the error was issued
             import inspect
             # Get the previous frame in the stack, otherwise it would be this function
             f = inspect.currentframe().f_back.f_code
@@ -225,13 +216,9 @@ class Pac(object):
 
         if event_type == "error":
             # We've queued a fatal event so we must exit installer_process process
-            # wait until queue is empty (is emptied in slides.py), then exit
+            # wait until queue is empty (is emptied in slides.py, in the GUI thread), then exit
             self.callback_queue.join()
             sys.exit(1)
-
-
-    ###################################################################
-    # Version functions
 
     def get_version(self):
         return "Cnchi running on pyalpm v%s - libalpm v%s" % (pyalpm.version(), pyalpm.alpmversion())
@@ -239,16 +226,17 @@ class Pac(object):
     def get_versions(self):
         return (pyalpm.version(), pyalpm.alpmversion())
 
-    ###################################################################
     # Callback functions
 
     def cb_conv(self, *args):
         pass
 
     def cb_totaldl(self, total_size):
+        """ Stores total download size for use in cb_progress """
         self.total_download_size = total_size
 
     def cb_event(self, ID, event, tupel):
+        """ Converts action ID to descriptive text and enqueues it to the events queue """
         action = ""
 
         if ID is 1:
@@ -281,6 +269,7 @@ class Pac(object):
             self.queue_event('action', action)
 
     def cb_log(self, level, line):
+        """ Log pyalpm warning and error messages """
         _logmask = pyalpm.LOG_ERROR | pyalpm.LOG_WARNING
 
         # Only manage error and warning messages
@@ -297,6 +286,7 @@ class Pac(object):
         #    pass
 
     def cb_progress(self, _target, _percent, n, i):
+        """ Calculates progress and enqueues events with the information """
         if _target:
             target = _("Installing %s (%d/%d)") % (_target, i, n)
             self.queue_event('global_percent', i / n)
@@ -311,32 +301,38 @@ class Pac(object):
         self.queue_event('percent', percent)
 
     def cb_dl(self, filename, tx, total):
+        """ Shows downloading progress """
         # Check if a new file is coming
-        # (if filename has changed or total file size has changed)
         if filename != self.last_dl_filename or self.last_dl_total != total:
             # Yes, new file
             self.last_dl_filename = filename
             self.last_dl_total = total
             self.last_dl_progress = 0
-            progress = 0
-            text = _("Downloading %s") % filename
-            self.queue_event('action', text)
-            self.queue_event('percent', progress)
 
-            # if pacman is just updating databases,
-            # total_download_size will be zero
-            if self.total_download_size > 0:
+            # If pacman is just updating databases total_download_size will be zero
+            if self.total_download_size == 0:
+                if filename.endswith(".db"):
+                    filename = filename[:-3]
+                text = _("Updating %s database") % filename
+            else:
+                if filename.endswith(".xz"):
+                    filename = filename[:-3]
+                text = _("Downloading %s") % filename
                 global_percent = self.total_downloaded / self.total_download_size
                 self.queue_event('global_percent', global_percent)
                 self.total_downloaded += total
+
+            self.queue_event('action', text)
+            self.queue_event('percent', 0)
         else:
             # Compute a progress indicator
             if self.last_dl_total > 0:
                 progress = tx / self.last_dl_total
             else:
-                # if total is unknown, use log(kBytes)²/2
+                # If total is unknown, use log(kBytes)²/2
                 progress = (math.log(1 + tx / 1024) ** 2 / 2) / 100
 
+            # Update progress only if it has grown
             if progress > self.last_dl_progress:
                 self.last_dl_progress = progress
                 #text = _("Downloading %s: %d/%d") % (filename, tx, total)
