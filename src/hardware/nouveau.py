58e595f2a242ef4a1de32cb2c9115b0b6b34e665
karasu <gustaucastells@gmail.com>
2014-01-20 10:59:25 +0100

    Added some nvidia cards (more cards need to be added)

--- src/hardware/nouveau.py
+++ src/hardware/nouveau.py
@@ -28,7 +28,37 @@ import os
 
 CLASS_NAME = "Nouveau"
 
-DEVICES = []
+# Supported cards http://nouveau.freedesktop.org/wiki/CodeNames/
+# NV04	Riva TNT, TNT2	Fahrenheit
+# NV10	GeForce 256, GeForce 2, GeForce 4 MX	Celsius
+# NV20	GeForce 3, GeForce 4 Ti	Kelvin
+# NV30	GeForce 5 / GeForce FX	Rankine
+# NV40	GeForce 6, GeForce 7	Curie
+# NV50	GeForce 8, GeForce 9, GeForce 100, GeForce 200, GeForce 300	Tesla
+# NVC0	GeForce 400, GeForce 500	Fermi
+# NVE0	GeForce 600, GeForce 700, GeForce GTX Titan	Kepler
+
+
+# TODO: Add all devices NOT FINISHED!
+
+DEVICES = [
+('0x10DE', '0x0020'), # Riva TNT
+('0x10DE', '0x002D'), # Riva TNT2
+('0x10DE', '0x0029'), # Riva TNT2 Ultra
+('0x10DE', '0x002B'), # Riva TNT2
+('0x10DE', '0x00A0'), # Aladdin TNT2
+('0x10DE', '0x0101'), # GeForce DDR
+('0x10DE', '0x0103'), # Quadro
+('0x10DE', '0x0110'), # GeForce2 MX
+('0x10DE', '0x0111'), # GeForce2 MMX
+('0x10DE', '0x0112'), # GeForce2 Go
+('0x10DE', '0x0113'), # Quadro 2
+('0x10DE', '0x9876'), # GeForce2 MX
+('0x10DE', '0x0150'), # GeForce2 GTS
+('0x10DE', '0x0151'), # GeForce2 Ti
+('0x10DE', '0x0152'), # GeForce2 Ultra
+('0x10DE', '0x0153') # Quadro2 Pro
+]
 
 class Nouveau(Hardware):
     def __init__(self):
