db83e3a8d39d51df74836919764adb72beaeda75
karasu <gustaucastells@gmail.com>
2014-01-27 11:01:02 +0100

    Added progress info to auto_partition. NEEDS TESTING

--- src/installation/auto_partition.py
+++ src/installation/auto_partition.py
@@ -177,8 +177,7 @@ class AutoPartition(object):
             command = mkfs[fs_type]
 
             try:
-                subprocess.check_call(command.split())
-                #self._check_call(command.split())
+                self._check_call(command.split())
             except subprocess.CalledProcessError as err:
                 txt = _("Can't create file system %s") % fs_type
                 logging.error(txt)
@@ -359,7 +358,7 @@ class AutoPartition(object):
         subprocess.check_call(command)
         try:
             self.progress += 0.1
-            self.callback_queue.put_nowait(('percent', progress))
+            self.callback_queue.put_nowait(('percent', self.progress))
         except queue.Full:
             pass
 
@@ -443,81 +442,81 @@ class AutoPartition(object):
         if self.uefi:
             # GPT (GUID) is supported only by 'parted' or 'sgdisk'
             # clean partition table to avoid issues!
-            subprocess.check_call(["sgdisk", "--zap", device])
+            self._check_call(["sgdisk", "--zap", device])
             
             # Clear all magic strings/signatures - mdadm, lvm, partition tables etc.
-            subprocess.check_call(["dd", "if=/dev/zero", "of=%s" % device, "bs=512", "count=2048", "status=noxfer"])
-            subprocess.check_call(["wipefs", "-a", device])
+            self._check_call(["dd", "if=/dev/zero", "of=%s" % device, "bs=512", "count=2048", "status=noxfer"])
+            self._check_call(["wipefs", "-a", device])
             # Create fresh GPT
-            subprocess.check_call(["sgdisk", "--clear", device])
+            self._check_call(["sgdisk", "--clear", device])
             # Inform the kernel of the partition change. Needed if the hard disk had a MBR partition table.
-            subprocess.check_call(["partprobe", device])
+            self._check_call(["partprobe", device])
             # Create actual partitions
-            subprocess.check_call(['sgdisk --set-alignment="2048" --new=1:1M:+%dM --typecode=1:EF02 --change-name=1:BIOS_GRUB %s'
+            self._check_call(['sgdisk --set-alignment="2048" --new=1:1M:+%dM --typecode=1:EF02 --change-name=1:BIOS_GRUB %s'
                 % (gpt_bios_grub_part_size, device)], shell=True)
-            subprocess.check_call(['sgdisk --set-alignment="2048" --new=2:0:+%dM --typecode=2:EF00 --change-name=2:UEFI_SYSTEM %s'
+            self._check_call(['sgdisk --set-alignment="2048" --new=2:0:+%dM --typecode=2:EF00 --change-name=2:UEFI_SYSTEM %s'
                 % (uefisys_part_size, device)], shell=True)
-            subprocess.check_call(['sgdisk --set-alignment="2048" --new=3:0:+%dM --typecode=3:8300 --attributes=3:set:2 --change-name=3:ANTERGOS_BOOT %s'
+            self._check_call(['sgdisk --set-alignment="2048" --new=3:0:+%dM --typecode=3:8300 --attributes=3:set:2 --change-name=3:ANTERGOS_BOOT %s'
                 % (boot_part_size, device)], shell=True)
 
             if self.lvm:
-                subprocess.check_call(['sgdisk --set-alignment="2048" --new=4:0:+%dM --typecode=4:8E00 --change-name=4:ANTERGOS_LVM %s'
+                self._check_call(['sgdisk --set-alignment="2048" --new=4:0:+%dM --typecode=4:8E00 --change-name=4:ANTERGOS_LVM %s'
                     % (lvm_pv_part_size, device)], shell=True)
             else:
-                subprocess.check_call(['sgdisk --set-alignment="2048" --new=4:0:+%dM --typecode=4:8200 --change-name=4:ANTERGOS_SWAP %s'
+                self._check_call(['sgdisk --set-alignment="2048" --new=4:0:+%dM --typecode=4:8200 --change-name=4:ANTERGOS_SWAP %s'
                     % (swap_part_size, device)], shell=True)
-                subprocess.check_call(['sgdisk --set-alignment="2048" --new=5:0:+%dM --typecode=5:8300 --change-name=5:ANTERGOS_ROOT %s'
+                self._check_call(['sgdisk --set-alignment="2048" --new=5:0:+%dM --typecode=5:8300 --change-name=5:ANTERGOS_ROOT %s'
                     % (root_part_size, device)], shell=True)
 
                 if self.home:
-                    subprocess.check_call(['sgdisk --set-alignment="2048" --new=6:0:+%dM --typecode=6:8300 --change-name=5:ANTERGOS_HOME %s'
+                    self._check_call(['sgdisk --set-alignment="2048" --new=6:0:+%dM --typecode=6:8300 --change-name=5:ANTERGOS_HOME %s'
                         % (home_part_size, device)], shell=True)
 
             logging.debug(check_output("sgdisk --print %s" % device))
         else:
             # Start at sector 1 for 4k drive compatibility and correct alignment
             # Clean partitiontable to avoid issues!
-            subprocess.check_call(["dd", "if=/dev/zero", "of=%s" % device, "bs=512", "count=2048", "status=noxfer"])
-            subprocess.check_call(["wipefs", "-a", device])
+            self._check_call(["dd", "if=/dev/zero", "of=%s" % device, "bs=512", "count=2048", "status=noxfer"])
+            self._check_call(["wipefs", "-a", device])
 
             # Create DOS MBR with parted
-            subprocess.check_call(["parted", "-a", "optimal", "-s", device, "mktable", "msdos"])
+            self._check_call(["parted", "-a", "optimal", "-s", device, "mktable", "msdos"])
 
             # Create boot partition (all sizes are in MB)
-            subprocess.check_call(["parted", "-a", "optimal", "-s", device, "mkpart", "primary", "1", str(boot_part_size)])
+            self._check_call(["parted", "-a", "optimal", "-s", device, "mkpart", "primary", "1", str(boot_part_size)])
             # Set boot partition as bootable
-            subprocess.check_call(["parted", "-a", "optimal", "-s", device, "set", "1", "boot", "on"])
+            self._check_call(["parted", "-a", "optimal", "-s", device, "set", "1", "boot", "on"])
 
             if self.lvm:
                 start = boot_part_size
                 end = start + lvm_pv_part_size
                 # Create partition for lvm (will store root, swap and home (if desired) logical volumes)
-                subprocess.check_call(["parted", "-a", "optimal", "-s", device, "mkpart", "primary", str(start), "100%"])
+                self._check_call(["parted", "-a", "optimal", "-s", device, "mkpart", "primary", str(start), "100%"])
                 # Set lvm flag
-                subprocess.check_call(["parted", "-a", "optimal", "-s", device, "set", "2", "lvm", "on"])
+                self._check_call(["parted", "-a", "optimal", "-s", device, "set", "2", "lvm", "on"])
             else:
                 # Create swap partition
                 start = boot_part_size
                 end = start + swap_part_size
-                subprocess.check_call(["parted", "-a", "optimal", "-s", device, "mkpart", "primary", "linux-swap",
+                self._check_call(["parted", "-a", "optimal", "-s", device, "mkpart", "primary", "linux-swap",
                     str(start), str(end)])
 
                 # Create root partition
                 start = end
                 end = start + root_part_size
-                subprocess.check_call(["parted", "-a", "optimal", "-s", device, "mkpart", "primary",
+                self._check_call(["parted", "-a", "optimal", "-s", device, "mkpart", "primary",
                     str(start), str(end)])
 
                 if self.home:
                     # Create home partition
                     start = end
-                    subprocess.check_call(["parted", "-a", "optimal", "-s", device, "mkpart", "primary",
+                    self._check_call(["parted", "-a", "optimal", "-s", device, "mkpart", "primary",
                         str(start), "100%"])
 
         printk(True)
 
         # Wait until /dev initialized correct devices
-        subprocess.check_call(["udevadm", "settle", "--quiet"])
+        self._check_call(["udevadm", "settle", "--quiet"])
 
         (efi_device, boot_device, swap_device, root_device, luks_devices, lvm_device, home_device) = self.get_devices()
 
