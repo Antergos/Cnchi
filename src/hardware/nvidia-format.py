849dcc6bcd72ee4de12127d0e2be18ac4fd0ef8a
karasu <karasu@cinnarch.com>
2014-01-30 13:52:44 +0100

    Finished nvidia pci device list

--- src/hardware/nvidia-format.py
+++
@@ -1,9 +0,0 @@
-with open("nvidia.txt", "r") as nvidia_txt:
-    txt = nvidia_txt.readlines()
-    
-with open("nvidia-out.py", "w") as nvidia:
-    for line in txt:
-        line = line.rtrim().split()
-        pciid = line.pop(0)
-        line = ', '.join(line)
-        # NOT FINISHED
