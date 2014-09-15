ed8377a6d6ff8cb2dba6842b948cec1f41c32332
karasu <karasu@localhost.localdomain>
2014-09-15 11:35:29 +0200

    Added debian install commands (just for testing purposes)

---
+++ install-debian.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get install python3 python-cairo-dev python-gobject-dev
+sudo apt-get install python-dbus python3-mako hdparm upower
+sudo apt-get install parted dosfstools mtools ntfs-3g ntfsprogs
+
+# Not in Debian:
+# python3-parted (pyparted)
+# python3-alpm (pyalpm)
+# libtimezonemap
+# python3-webkit
+
