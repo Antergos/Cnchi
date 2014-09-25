bbeb921fdcebe70cea47696c082dd607e33e856a
karasu <gustaucastells@gmail.com>
2014-09-25 12:03:55 +0200

    Forgot introspection typelib for libtimezonemap

--- utils/install-debian.sh
+++ utils/install-debian.sh
@@ -3,7 +3,7 @@ sudo apt-get -y install git glade geany
 sudo apt-get -y install python3 python-cairo-dev python-gobject-dev
 sudo apt-get -y install python-dbus python3-mako hdparm upower
 sudo apt-get -y install parted dosfstools mtools ntfs-3g
-sudo apt-get -y install libtimezonemap1-dev
+sudo apt-get -y install libtimezonemap1-dev gir1.2-timezonemap-1.0
 
 # Not in Debian:
 # python3-parted (pyparted)
