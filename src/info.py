6bc244d497438d9edd0707da7806e771a8193bfd
Dustin Falgout <dustin@falgout.us>
2014-11-21 01:28:24 -0600

    fix version

--- src/info.py
+++ src/info.py
@@ -22,6 +22,6 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.6.2"
-CNCHI_LAST_STABLE = "0.6.2"
+CNCHI_VERSION = "0.6.18"
+CNCHI_LAST_STABLE = "0.6.18"
 CNCHI_WEBSITE = "http://www.antergos.com"
