9a91b5df1c44ef8c2c77b6987999e864f3b53ce4
karasu <gustaucastells@gmail.com>
2014-11-29 20:21:39 +0100

    Updated testing.sh

--- scripts/testing.sh
+++ scripts/testing.sh
@@ -3,26 +3,84 @@
 previous="/tmp/dev-setup"
 uefi="/sys/firmware/efi"
 vbox_chk="$(hwinfo --gfxcard | grep -o -m 1 "VirtualBox")"
-notify="$1"
+arg="$1"
 notify_user () {
 
         notify-send -t 10000 -a "Cnchi" -i /usr/share/cnchi/data/antergos-icon.png "$1"
 }
 
+do_update () {
+
+# Update Cnchi with latest testing code
+	echo "Removing existing Cnchi..."
+	killall python
+	mv  /usr/share/cnchi /usr/share/cnchi.old;
+	if [[ ${development} = "True" ]]; then
+
+		notify_user "Getting latest version of Cnchi from development branch..."
+		echo "Getting latest version of Cnchi from development branch..."
+		# Check commandline arguments to choose repo
+		#if [ "$1" = "-d" ] || [ "$1" = "--dev-repo" ]; then
+		#	git clone https://github.com/"$2"/Cnchi.git cnchi;
+		#else
+		#	git clone https://github.com/Antergos/Cnchi.git cnchi;
+		#fi
+		cd /tmp
+		{ wget http://antergos.org/cnchi.tar && tar -xf cnchi.tar && cp -R cnchi /usr/share && rm cnchi.tar \
+		 	&& rm -Rf cnchi && cd /usr/share/cnchi && return 0; } || \
+		{ mv  /usr/share/cnchi.old /usr/share/cnchi && notify_user "Something went wrong. Update failed." \
+		 && return 1; }
+	else
+		notify_user "Getting latest version of Cnchi from stable branch..."
+		echo "Getting latest version of Cnchi from stable branch..."
+		{ pacman -Syy --noconfirm cnchi && return 0; } || { mv  /usr/share/cnchi.old /usr/share/cnchi \
+		&& notify_user "Something went wrong. Update failed." && return 1; }
+	fi
+
+}
+
+start_cnchi () {
+
+	# Start Cnchi with appropriate options
+	notify_user "Starting Cnchi..."
+	echo "Starting Cnchi..."
+	if [[ ${development} = "True" ]]; then
+		cnchi -d -v -z -p /usr/share/cnchi/data/packages.xml &
+		exit 0;
+
+
+	else
+
+		cnchi -d -v &
+		exit 0;
+
+	fi
+
+}
+
+if [[ ${arg} = "development" ]]; then
+	development=True
+else
+	stable=True
+fi
 # Check if this is the first time we are executed.
 if ! [ -f "${previous}" ]; then
 	touch ${previous};
 	# Find the best mirrors (fastest and latest)
-    notify_user "Selecting the best mirrors..."
-	echo "Selecting the best mirrors..."
-	echo "Testing Arch mirrors..."
-	reflector -p http -l 30 -f 5 --save /etc/pacman.d/mirrorlist;
-	echo "Done."
-	sudo -u antergos wget http://antergos.info/antergos-mirrorlist
-	echo "Testing Antergos mirrors..."
-	rankmirrors -n 0 -r antergos antergos-mirrorlist > /tmp/antergos-mirrorlist
-	cp /tmp/antergos-mirrorlist /etc/pacman.d/
-	echo "Done."
+#    notify_user "Selecting the best mirrors..."
+#	echo "Selecting the best mirrors..."
+#	echo "Testing Arch mirrors..."
+#	reflector -p http -l 30 -f 5 --save /etc/pacman.d/mirrorlist;
+#	echo "Done."
+#	sudo -u antergos wget http://antergos.info/antergos-mirrorlist
+#	echo "Testing Antergos mirrors..."
+#	rankmirrors -n 0 -r antergos antergos-mirrorlist > /tmp/antergos-mirrorlist
+#	cp /tmp/antergos-mirrorlist /etc/pacman.d/
+#	echo "Done."
+
+	pacman-key --init
+	pacman-key --populate archlinux antergos
+
 	# Install any packages that haven't been added to the iso yet but are needed.
 	notify_user "Installing missing packages..."
 	echo "Installing missing packages..."
@@ -41,24 +99,6 @@ if ! [ -f "${previous}" ]; then
 	else
 		modprobe -a f2fs dm-mod;
 	fi
-	# Update Cnchi with latest testing code
-	echo "Removing existing Cnchi..."
-	rm -R /usr/share/cnchi;
-	cd /usr/share;
-	notify_user "Getting latest version of Cnchi from testing branch..."
-	echo "Getting latest version of Cnchi from testing branch..."
-	# Check commandline arguments to choose repo
-	#if [ "$1" = "-d" ] || [ "$1" = "--dev-repo" ]; then
-	#	git clone https://github.com/"$2"/Cnchi.git cnchi;
-	#else
-	#	git clone https://github.com/Antergos/Cnchi.git cnchi;
-	#fi
-	cd /tmp
-	wget http://antergos.org/cnchi.tar
-	tar -xvf cnchi.tar
-	cp -R cnchi/tmp/cnchi /usr/share
-	rm cnchi.tar && rm -Rf cnchi
-	cd /usr/share/cnchi
 	
 else
     notify_user "Previous testing setup detected, skipping downloads..."
@@ -66,40 +106,11 @@ else
 	echo "Verifying that nothing is mounted from a previous install attempt."
 	umount -lf /install/boot >/dev/null 2&>1
 	umount -lf /install >/dev/null 2&>1
-	# Check for changes on github since last time script was executed
-	# Update Cnchi with latest testing code
-	notify_user "Getting latest version of Cnchi from testing branch..."
-	echo "Getting latest version of Cnchi from testing branch..."
-	#cd /usr/share/cnchi
-	#git pull origin master;
-	cd /tmp
-	wget http://antergos.org/cnchi.tar
-	tar -xvf cnchi.tar
-	cp -R cnchi/tmp/cnchi /usr/share
-	cd /usr/share/cnchi
+
 fi
 
-# Start Cnchi with appropriate options
-notify_user "Starting Cnchi..."
-echo "Starting Cnchi..."
-# Are we using an alternate PKG cache?
-# TODO Remove this nonsense and use proper command argument processing
-if [ "$1" != "-d" ] && [ "$1" != "--dev-repo" ] && [ "$1" != "" ]; then
-    if [ "$1" = "-c" ] || [ "$1" = "--cache" ]; then
-        if [ "$2" != "" ]; then
-            if [ "$3" = "-z" ]; then
-                cnchi -d -v -z -c "$2" -p /usr/share/cnchi/data/packages.xml & exit 0;
-            else
-                cnchi -d -v -c "$2" -p /usr/share/cnchi/data/packages.xml & exit 0;
-            fi
-        else
-            cnchi -d -v -c /media/sf_data/PKG-CACHE/pkg/ -p /usr/share/cnchi/data/packages.xml & exit 0;
-        fi
-    fi
-elif [ "$1" = "-d" ] || [ "$1" = "--dev-repo" ]; then
-    cnchi -d -v -z -p /usr/share/cnchi/data/packages.xml & exit 0;
-else
-    cnchi -d -v -p /usr/share/cnchi/data/packages.xml & exit 0;
-fi
+
+do_update && start_cnchi
+
 
 exit 1;
