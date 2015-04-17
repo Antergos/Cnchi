25e50122688588825cce5515150a7c88132ee4a6
karasu <karasu@antergos.com>
2015-04-17 19:32:38 +0200

    It works! Now it needs to be adapted (like timezonemap)

--- cnchi/misc/cheese.py
+++ cnchi/misc/cheese.py
@@ -25,15 +25,19 @@
 import sys
 import logging
 
+import gi
+gi.require_version('Gst', '1.0')
+
 from gi.repository import GtkClutter
-GtkClutter.init([])
-
 from gi.repository import Clutter, GObject, Gtk, Gst, GLib
-
 from gi.repository import Cheese
 
 
-class VideoBox(GtkClutter.Embed):
+def camera_box_init():
+    Gst.init(None)
+    GtkClutter.init([])
+
+class CameraBox(GtkClutter.Embed):
     __gtype_name__ = 'VideoBox'
 
     # __gsignals__ = {'location-changed': (GObject.SignalFlags.RUN_LAST, None, (object,))}
@@ -47,11 +51,10 @@ class VideoBox(GtkClutter.Embed):
         self.stage.set_size(width, height)
 
         self.layout_manager = Clutter.BoxLayout()
-        self.textures_box = Clutter.Actor(layout_manager=self.layout_manager)
-        self.stage.add_actor(self.textures_box)
+        self.container = Clutter.Actor(layout_manager=self.layout_manager)
+        self.stage.add_actor(self.container)
 
         self.video_texture = Clutter.Texture.new()
-
         self.video_texture.set_keep_aspect_ratio(True)
         self.video_texture.set_size(width, height)
         self.layout_manager.pack(
@@ -85,99 +88,22 @@ class VideoBox(GtkClutter.Embed):
         device_monitor.coldplug()
 
         self.stage.show()
-        # Clutter.main()
 
 if __name__ == "__main__":
-    # Gst.init(None)
-    # Clutter.init(sys.argv)
-    # app = VideoBox()
-
-    # GtkClutter.init(ref args);
+    camera_box_init()
 
     win = Gtk.Window()
-    video_box = VideoBox()
-    win.add(video_box)
+    camera_box = CameraBox()
+    win.add(camera_box)
     win.show_all()
+
+    def exit_button(widget, data=None):
+        sys.exit(0)
+
+    win.connect('delete-event', exit_button)
 
     import signal    # enable Ctrl-C since there is no menu to quit
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     Gtk.main()
-
-
-
-
-
-'''
-from gi.repository import GtkClutter
-GtkClutter.init([])
-from gi.repository import Clutter, Gtk, Gdk
-
-GtkClutter.init([])
-
-class MyCanvas(GtkClutter.Embed):
-    __gtype_name__ = 'MyCanvas'
-
-    def __init__(self):
-        super(MyCanvas, self).__init__()
-
-        self.stage = self.get_stage()
-
-        self.rect = MyRect()
-        self.stage.connect('key-press-event', self.key_press) # This Works!
-        self.stage.add_actor(self.rect)
-        self.rect.set_position(10, 10)
-
-        self.show_all()
-
-    def key_press(self, widget, event):
-        print widget, event
-
-
-class MyRect(Clutter.Rectangle):
-    __gtype_name__ = 'MyRect'
-
-    def __init__(self):
-        super(MyRect, self).__init__()
-        self.set_color(Clutter.Color.new(255, 255, 255, 0))
-        self.set_border_color(Clutter.Color.new(255, 255, 255, 255))
-        self.set_border_width(1)
-        self.set_size(200, 200)
-
-        self.set_reactive(True)
-
-    # None of the following callbacks work
-    # Not even if I explicitly connect signals like 
-    # self.connect('button-press-event', self.on_button_press)
-
-    def do_button_press_event(self, event):
-        print event
-
-    def do_motion_event(self, event):
-        print event
-
-
-class MyWindow(Gtk.Window):
-    __gtype_name__ = 'MyWindow'
-
-    def __init__(self):
-        super(MyWindow, self).__init__()
-        self.canvas = AnnotateCanvas()
-        self.set_events(Gdk.EventMask.BUTTON_PRESS_MASK | Gdk.EventMask.BUTTON1_MOTION_MASK)
-        self.set_size_request(500, 500)
-
-        vbox_main = Gtk.VBox()
-        scrolledwin = Gtk.ScrolledWindow()
-        scrolledwin.add_with_viewport(self.canvas)
-        vbox_main.pack_end(scrolledwin, True, True, 0)
-        self.add(vbox_main)
-
-    def run(self):
-        self.show_all()
-        self.loop = GObject.MainLoop()
-        self.loop.run()
-
-
-app = MyWindow()
-app.run()'''
 
