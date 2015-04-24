7037c5796b81818918d1a0b2cf350b8effd5d2d0
plumpudding <9lum9u66ing@gmail.com>
2015-04-24 22:29:25 +0200

    fix second problem with encryption and gummiboot

--- cnchi/installation/bootloader.py
+++ cnchi/installation/bootloader.py
@@ -488,7 +488,7 @@ class Bootloader(object):
                 key = "cryptkey=UUID={0}:ext2:/.keyfile-root".format(self.boot_uuid)
 
             root_uuid_line = "cryptdevice=UUID={0}:{1} {2} root=UUID={3} rw"
-            root_uuid_line = root_uuid_line.format(root_uuid, luks_root_volume, key, root_uuid)
+            root_uuid_line = root_uuid_line.format(root_uuid, luks_root_volume, key, luks_root_volume)
 
             conf['default'] = []
             conf['default'].append("title\tAntergos\n")
