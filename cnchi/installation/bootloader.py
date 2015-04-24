467b8e76b9f82485c952a8ddcd9d225467552e3e
plumpudding <9lum9u66ing@gmail.com>
2015-04-24 23:10:19 +0200

    Update bootloader.py

--- cnchi/installation/bootloader.py
+++ cnchi/installation/bootloader.py
@@ -488,7 +488,7 @@ class Bootloader(object):
                 key = "cryptkey=UUID={0}:ext2:/.keyfile-root".format(self.boot_uuid)
 
             root_uuid_line = "cryptdevice=UUID={0}:{1} {2} root={3} rw"
-            root_uuid_line = root_uuid_line.format(root_uuid, luks_root_volume, key, luks_root_volume)
+            root_uuid_line = root_uuid_line.format(root_uuid, luks_root_volume, key, luks_root_device)
 
             conf['default'] = []
             conf['default'].append("title\tAntergos\n")
