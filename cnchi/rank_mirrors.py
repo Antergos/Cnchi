bc95f93c92252814df02e238f2d0d4875e576d1b
Dustin Falgout <dustin@falgout.us>
2015-07-18 11:04:12 -0500

    we can use sourceforge now that I updated the mirrorlist

--- cnchi/rank_mirrors.py
+++ cnchi/rank_mirrors.py
@@ -97,10 +97,6 @@ class AutoRankmirrorsThread(threading.Thread):
                     # Uncomment Antergos mirror
                     lines[i] = lines[i].lstrip("#")
 
-                # sourceforge server does not get updated as often as necessary
-                if "sourceforge" in lines[i]:
-                    lines[i] = "#" + lines[i]
-
             with misc.raised_privileges():
                 # Backup original file
                 shutil.copy(self.antergos_mirrorlist, self.antergos_mirrorlist + ".cnchi_backup")
