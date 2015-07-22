569f7ffe1696acb27a7ca4c420a4b90d1b271da0
karasu <gustaucastells@gmail.com>
2015-07-23 01:20:34 +0200

    Updated cnchi.pot file

--- utils/download_package_list.py
+++ utils/download_package_list.py
@@ -2,7 +2,7 @@ import urllib.request
 import urllib.error
 
 try:
-    packages_xml = urllib.request.urlopen('http://install.antergos.com/packages-0.4.xml')
+    packages_xml = urllib.request.urlopen('http://install.antergos.com/packages-0.8.xml')
 except urllib.error.URLError as e:
     # If the installer can't retrieve the remote file, try to install with a local
     # copy, that may not be updated
