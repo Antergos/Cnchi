ab0bc80538e9c85de3725859597ccdd9992f10be
Dustin Falgout <dustin@falgout.us>
2015-08-15 05:00:27 -0500

    Update license statement to require all atributions and copyright notices be included in any and all code sourced from Cnchi

--- cnchi/info.py
+++ cnchi/info.py
@@ -9,7 +9,7 @@
 #
 #  Cnchi is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
+#  the Free Software Foundation; either version 3 of the License, or
 #  (at your option) any later version.
 #
 #  Cnchi is distributed in the hope that it will be useful,
@@ -17,14 +17,19 @@
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
+#  The following additional terms are in effect as per Section 7 of the license:
+#
+#  The preservation of all legal notices and author attributions in
+#  the material or in the Appropriate Legal Notices displayed
+#  by works containing it is required.
+#
 #  You should have received a copy of the GNU General Public License
-#  along with Cnchi; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
+#  along with Cnchi; If not, see <http://www.gnu.org/licenses/>.
+
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.9.249"
+CNCHI_VERSION = "0.9.250"
 CNCHI_WEBSITE = "http://www.antergos.com"
 
 if __name__ == '__main__':
