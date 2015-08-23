32f91c7d2a69c8c91aa8685c7671dd7ae5ecc57c
Dustin Falgout <dustin@falgout.us>
2015-08-23 05:21:36 -0500

    fix packages.xml url. add release_stage global variable. (cherry-picked from lots0logs/cnchi-dev@b13a5a86855f171b432014ee325214b7da65cb15)

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,8 +29,9 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.10.25"
+CNCHI_VERSION = "0.10.29"
 CNCHI_WEBSITE = "http://www.antergos.com"
+CNCHI_RELEASE_STAGE = "production"
 
 if __name__ == '__main__':
     print(CNCHI_VERSION)
