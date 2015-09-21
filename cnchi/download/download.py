bfa404eaf4ae4082fc31c7f222be2f9b30b578d5
Dustin Falgout <dustin@falgout.us>
2015-09-21 07:40:46 -0500

    oops :grimacing:

--- cnchi/download/download.py
+++ cnchi/download/download.py
@@ -174,18 +174,14 @@ class DownloadPackages(object):
 
                 # Get metalink info
                 metalink_info = ml.get_info(metalink)
-                #logging.debug(metalink_info)
 
 
                 # Update downloads list with the new info from the processed metalink
                 for key in metalink_info:
-                    logging.debug(key)
                     if key not in self.metalinks:
                         self.metalinks[key] = metalink_info[key]
 
                 # Show progress to the user
-                logging.debug(self.metalinks)
-                sys.exit()
                 processed_packages += 1
                 percent = round(float(processed_packages / total_packages), 2)
                 self.queue_event('percent', str(percent))
