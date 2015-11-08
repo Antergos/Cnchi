d57fc54bed17947641e33bc953caea2b5da8f5ad
karasu <gustaucastells@gmail.com>
2015-11-08 13:10:18 +0100

    Add pl to cnchi.desktop

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.13.143"
+CNCHI_VERSION = "0.13.144"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "development"
 
