ff498ceb7b994579ff3086a703bbaabbb0cc1211
karasu <gustaucastells@gmail.com>
2015-12-01 15:28:56 +0100

    pep8 on language

--- cnchi/language.py
+++ cnchi/language.py
@@ -3,28 +3,28 @@
 #
 #  language.py
 #
-#  Copyright © 2013-2015 Antergos
+# Copyright © 2013-2015 Antergos
 #
-#  This file is part of Cnchi.
+# This file is part of Cnchi.
 #
-#  Cnchi is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  (at your option) any later version.
+# Cnchi is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
 #
-#  Cnchi is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+# Cnchi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#  The following additional terms are in effect as per Section 7 of the license:
+# The following additional terms are in effect as per Section 7 of the license:
 #
-#  The preservation of all legal notices and author attributions in
-#  the material or in the Appropriate Legal Notices displayed
-#  by works containing it is required.
+# The preservation of all legal notices and author attributions in
+# the material or in the Appropriate Legal Notices displayed
+# by works containing it is required.
 #
-#  You should have received a copy of the GNU General Public License
-#  along with Cnchi; If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with Cnchi; If not, see <http://www.gnu.org/licenses/>.
 
 
 from gi.repository import Gtk
@@ -57,7 +57,10 @@ class Language(GtkBaseBox):
         data_dir = self.settings.get('data')
 
         self.current_locale = locale.getdefaultlocale()[0]
-        self.language_list = os.path.join(data_dir, "locale", "languagelist.txt.gz")
+        self.language_list = os.path.join(
+            data_dir,
+            "locale",
+            "languagelist.txt.gz")
         self.set_languages_list()
 
         image1 = self.ui.get_object("image1")
@@ -75,7 +78,9 @@ class Language(GtkBaseBox):
         if listbox_row is not None:
             for vbox in listbox_row:
                 for label in vbox.get_children():
-                    current_language, sorted_choices, display_map = i18n.get_languages(self.language_list)
+                    (current_language,
+                        sorted_choices,
+                        display_map) = i18n.get_languages(self.language_list)
                     lang = label.get_text()
                     lang_code = display_map[lang][1]
                     self.set_language(lang_code)
@@ -83,15 +88,19 @@ class Language(GtkBaseBox):
     def translate_ui(self):
         """ Translates all ui elements """
         txt_bold = _("Notice: The Cnchi Installer is beta software.")
-        # FIXME: Can't use an a html tag in the label. Causes an accessible GTK Assertion
-        txt = _("Cnchi is pre-release beta software that is under active development. "
-                "It does not yet properly handle RAID, btrfs subvolumes, or other advanced "
-                "setups. Please proceed with caution as data loss is possible!\n\n"
-                "If you find any bugs, please report them at {0}")
-        txt = txt.format("<a href='http://bugs.antergos.com'>bugs.antergos.com</a>")
-        txt_markup = "<span weight='bold'>{0}</span>\n\n{1}".format(txt_bold, txt)
+        # FIXME: Can't use an a html tag in the label
+        # (as we're running as root)
+        txt = _("<span weight='bold'>{0}</span>\n\n"
+                "Cnchi is pre-release beta software that is under active "
+                "development. It does not yet properly handle RAID, btrfs "
+                "subvolumes, or other advanced setups. Please proceed with "
+                "caution as data loss is possible!\n\n"
+                "If you find any bugs, please report them at "
+                "<a href='{1}'>{1}</a>")
+        url = "http://bugs.antergos.com"
+        txt = txt.format(txt_bold, url)
         label = self.ui.get_object("welcome_label")
-        label.set_markup(txt_markup)
+        label.set_markup(txt)
 
         label.set_hexpand(False)
         label.set_line_wrap(True)
@@ -112,7 +121,9 @@ class Language(GtkBaseBox):
     def set_languages_list(self):
         """ Load languages list """
         try:
-            current_language, sorted_choices, display_map = i18n.get_languages(self.language_list)
+            (current_language,
+                sorted_choices,
+                display_map) = i18n.get_languages(self.language_list)
         except FileNotFoundError as file_error:
             logging.error(file_error)
             sys.exit(1)
@@ -136,7 +147,9 @@ class Language(GtkBaseBox):
             lang.install()
             self.translate_ui()
         except IOError:
-            logging.warning("Can't find translation file for the %s language", locale_code)
+            logging.warning(
+                "Can't find translation file for the %s language",
+                locale_code)
 
     def select_default_row(self, language):
         for listbox_row in self.listbox.get_children():
@@ -154,8 +167,11 @@ class Language(GtkBaseBox):
                 for label in vbox.get_children():
                     lang = label.get_text()
 
-        current_language, sorted_choices, display_map = i18n.get_languages(self.language_list)
-        if len(lang) > 0:
+        (current_language,
+            sorted_choices,
+            display_map) = i18n.get_languages(self.language_list)
+
+        if lang:
             self.settings.set("language_name", display_map[lang][0])
             self.settings.set("language_code", display_map[lang][1])
 
@@ -168,7 +184,9 @@ class Language(GtkBaseBox):
         self.show_all()
 
         # Launch rank mirrors process to optimize Arch and Antergos mirrorlists
-        if not self.testing and not self.disable_rank_mirrors and not self.rank_mirrors_launched:
+        if (not self.testing and
+                not self.disable_rank_mirrors and
+                not self.rank_mirrors_launched):
             proc = AutoRankmirrorsProcess(self.settings)
             proc.daemon = True
             proc.name = "rankmirrors"
