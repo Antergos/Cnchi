a607c59a7f8b5399058f992df4b1ef4a1a441919
karasu <gustaucastells@gmail.com>
2015-12-03 22:37:41 +0100

    Split a big function. Needs testing!

--- cnchi/download/download_requests.py
+++ cnchi/download/download_requests.py
@@ -40,6 +40,7 @@ import socket
 import io
 import threading
 
+
 def get_md5(file_name):
     """ Gets md5 hash from a file """
     md5_hash = hashlib.md5()
@@ -94,7 +95,7 @@ class Download(object):
         md5hash = element.get('hash', False)
         if md5hash and md5hash != md5:
             logging.debug(
-                "MD5 hash of file %s does not match what's in the repo database! Cnchi will download it",
+                "MD5 hash of file %s does not match what's in the repository database!",
                 element['filename'])
             is_ok = False
         elif md5hash == md5 or not md5hash:
@@ -103,7 +104,7 @@ class Download(object):
                 'cache_pkgs_md5_check_failed',
                 element['identity'])
             logging.debug(
-                    'Checksum unavailable for cached package: %s',
+                    'Checksum unavailable for package: %s',
                     element['identity'])
             is_ok = True
         return is_ok
@@ -113,7 +114,7 @@ class Download(object):
         downloaded = 0
         total_downloads = len(downloads)
         download_error = False
-        needs_to_download = None
+        needs_to_download = False
 
         self.queue_event('downloads_progress_bar', 'show')
         self.queue_event('downloads_percent', '0')
@@ -148,7 +149,6 @@ class Download(object):
                     needs_to_download = True
                 else:
                     needs_to_download = False
-                    downloaded += 1
                     logging.debug(
                         "File %s already exists, Cnchi will not overwrite it",
                         element['filename'])
@@ -160,134 +160,37 @@ class Download(object):
                         xz_cache_dir,
                         element['filename'])
 
-                    if os.path.exists(dst_xz_cache_path):
+                    if (os.path.exists(dst_xz_cache_path) and
+                            self.is_hash_ok(element, dst_xz_cache_path)):
                         # We're lucky, the package is already downloaded
                         # in the cache the user has given us
-                        # Check the file's md5 hash
-
-                        if self.is_hash_ok(element, dst_xz_cache_path):
-                            copy_it = True
-                        else:
-                            copy_it = False
-
-                        if copy_it:
-                            logging.debug(
-                                "%s found in suplied xz packages' cache. Copying...",
-                                element['filename'])
-                            try:
-                                # shutil.copy(dst_xz_cache_path, dst_path)
-                                needs_to_download = False
-                                downloaded += 1
-                                # Get out of the for loop, as we managed
-                                # to find the package in this cache directory
-                                break
-                            except OSError as os_error:
-                                logging.debug(
-                                    "Error copying %s to %s : %s",
-                                    dst_xz_cache_path,
-                                    dst_path,
-                                    os_error)
-
-            if needs_to_download:
-                # Package wasn't previously downloaded or its md5 was wrong
-                # We'll have to download it
-                # Let's download our file using its url
-                for url in element['urls']:
-                    # Let's catch empty values as well as None just to be safe
-                    if not url:
-                        # Something bad has happened
-                        download_error = True
+                        # and its md5 checks out (if there is a md5)
                         logging.debug(
-                            "Package %s v%s has an empty url for this mirror",
-                            element['identity'],
-                            element['version'])
-                        continue
-                    percent = 0
-                    completed_length = 0
-                    start = time.perf_counter()
-                    try:
-                        # By default, get waits five minutes before
-                        # issuing a timeout, which is too much.
-                        req = requests.get(url, stream=True, timeout=30)
-                        if req.status_code == requests.codes.ok:
-                            # Get total file length
-                            try:
-                                total_length = int(req.headers.get('content-length'))
-                            except TypeError:
-                                total_length = 0
-                                logging.debug(
-                                    "Metalink for package %s has no size info",
-                                    element['identity'])
-
-                            with open(dst_path, 'wb') as xz_file:
-                                for data in req.iter_content(io.DEFAULT_BUFFER_SIZE):
-                                    if not data:
-                                        break
-                                    xz_file.write(data)
-                                    completed_length += len(data)
-                                    old_percent = percent
-                                    if total_length > 0:
-                                        percent = round(float(completed_length / total_length), 2)
-                                    else:
-                                        percent += 0.1
-                                    if old_percent != percent:
-                                        self.queue_event('percent', percent)
-                                    bps = completed_length // (time.perf_counter() - start)
-                                    if bps >= (1024 * 1024):
-                                        Mbps = bps / (1024 * 1024)
-                                        progress_text = "{0}%   {1:.2f} Mbps".format(int(percent * 100), Mbps)
-                                    elif bps >= 1024:
-                                        Kbps = bps / 1024
-                                        progress_text = "{0}%   {1:.2f} Kbps".format(int(percent * 100), Kbps)
-                                    else:
-                                        progress_text = "{0}%   {1:.2f} bps".format(int(percent * 100), bps)
-                                    self.queue_event('progress_bar_show_text', progress_text)
-
-                            # element['hash'] is not always available
-                            # that is why we have to check against None
-                            if element['hash'] is not None:
-                                md5 = get_md5(dst_path)
-                                if element['hash'] != md5:
-                                    # Wrong md5! Force to download it again
-                                    download_error = True
-                                    logging.debug(
-                                        "MD5 hash of file %s (%s) do not match! Cnchi will try another mirror.",
-                                        element['filename'],
-                                        url)
-                                    continue
-
-                            # If we've reached here let's assume it's ok
-                            download_error = False
-                            downloaded += 1
-
-                            # Copy downloaded xz file to the cache the user has provided, too.
-                            copy_to_cache_thread = CopyToCache(dst_path, self.xz_cache_dirs)
-                            copy_to_cache_threads += [copy_to_cache_thread]
-                            copy_to_cache_thread.start()
-
+                            "%s found in suplied xz packages' cache. Copying...",
+                            element['filename'])
+                        try:
+                            shutil.copy(dst_xz_cache_path, dst_path)
+                            needs_to_download = False
                             # Get out of the for loop, as we managed
-                            # to download the package
+                            # to find the package in this cache directory
                             break
-                        else:
-                            # requests failed to obtain the file. Wrong url?
-                            download_error = True
-                            msg = "Can't download {0}, Cnchi will try another mirror.".format(url)
-                            logging.debug(msg)
-                            continue
-                    except (socket.timeout, requests.exceptions.Timeout, requests.exceptions.ConnectionError) as connection_error:
-                        download_error = True
-                        msg = "Can't download {0} ({1}), Cnchi will try another mirror in a minute.".format(url, connection_error)
-                        logging.debug(msg)
-                        time.sleep(60) # delays for 60 seconds
-                        continue
+                        except OSError as os_error:
+                            logging.debug(
+                                "Error copying %s to %s : %s",
+                                dst_xz_cache_path,
+                                dst_path,
+                                os_error)
 
-                if download_error:
-                    # None of the mirror urls works.
-                    # Stop right here, so the user does not have to wait
-                    # to download the other packages.
-                    msg = "Can't download {0}, even after trying all available mirrors".format(element['filename'])
-                    logging.error(msg)
-                    return False
+            if needs_to_download and not self.download_package(element, dst_path):
+                # None of the mirror urls works.
+                # Stop right here, so the user does not have to wait
+                # to download the other packages.
+                msg = "Can't download {0}, even after trying all available mirrors"
+                msg = msg.format(element['filename'])
+                logging.error(msg)
+                return False
+            else:
+                downloaded += 1
 
             self.queue_event('progress_bar_show_text', '')
 
@@ -295,11 +198,115 @@ class Download(object):
             self.queue_event('downloads_percent', str(downloads_percent))
 
         # Wait until all xz packages are also copied to provided cache (if any)
-        for x in copy_to_cache_threads:
-            x.join()
+        for cache_thread in copy_to_cache_threads:
+            cache_thread.join()
 
         self.queue_event('downloads_progress_bar', 'hide')
         return True
+
+    def download_package(self, element, dst_path):
+        """ Package wasn't previously downloaded or its md5 was wrong
+            We'll have to download it
+            Let's download our file using its url """
+
+        download_error = True
+
+        for url in element['urls']:
+            # Let's catch empty values as well as None just to be safe
+            if not url:
+                # Something bad has happened, let's try another mirror
+                download_error = True
+                logging.debug(
+                    "Package %s v%s has an empty url for this mirror",
+                    element['identity'],
+                    element['version'])
+                continue
+            percent = 0
+            completed_length = 0
+            start = time.perf_counter()
+            try:
+                # By default, get waits five minutes before
+                # issuing a timeout, which is too much.
+                req = requests.get(url, stream=True, timeout=30)
+                if req.status_code == requests.codes.ok:
+                    # Get total file length
+                    try:
+                        total_length = int(req.headers.get('content-length'))
+                    except TypeError:
+                        total_length = 0
+                        logging.debug(
+                            "Metalink for package %s has no size info",
+                            element['identity'])
+
+                    with open(dst_path, 'wb') as xz_file:
+                        for data in req.iter_content(io.DEFAULT_BUFFER_SIZE):
+                            if not data:
+                                break
+                            xz_file.write(data)
+                            completed_length += len(data)
+                            old_percent = percent
+                            if total_length > 0:
+                                percent = float(completed_length / total_length)
+                                percent = round(percent, 2)
+                            else:
+                                percent += 0.1
+                            if old_percent != percent:
+                                self.queue_event('percent', percent)
+                            bps = completed_length // (time.perf_counter() - start)
+                            msg = self.format_progress_message(percent, bps)
+                            self.queue_event('progress_bar_show_text', msg)
+
+                    # Check hash of downloaded package
+                    if not self.is_hash_ok(element, dst_path):
+                        # Wrong md5! Force to download it again
+                        download_error = True
+                        logging.debug(
+                            "MD5 hash of file %s (%s) do not match! Cnchi will try another mirror.",
+                            element['filename'],
+                            url)
+                        continue
+
+                    # If we've reached here let's assume it's ok
+                    download_error = False
+
+                    # Copy downloaded xz file to the cache the user has provided, too.
+                    copy_to_cache_thread = CopyToCache(dst_path, self.xz_cache_dirs)
+                    copy_to_cache_threads += [copy_to_cache_thread]
+                    copy_to_cache_thread.start()
+
+                    # Get out of the for loop, as we managed
+                    # to download the package
+                    break
+                else:
+                    # requests failed to obtain the file. Wrong url?
+                    download_error = True
+                    msg = "Can't download {0}, Cnchi will try another mirror."
+                    msg = msg.format(url)
+                    logging.debug(msg)
+                    continue
+            except (socket.timeout,
+                    requests.exceptions.Timeout,
+                    requests.exceptions.ConnectionError) as connection_error:
+                download_error = True
+                msg = "Can't download {0} ({1}), Cnchi will try another mirror in a minute."
+                msg = msg.format(url, connection_error)
+                logging.debug(msg)
+                # delays for 60 seconds
+                time.sleep(60)
+                continue
+        return download_error
+
+    def format_progress_message(percent, bps):
+        """ Formats speed message information """
+        if bps >= (1024 * 1024):
+            Mbps = bps / (1024 * 1024)
+            msg = "{0}%   {1:.2f} Mbps".format(int(percent * 100), Mbps)
+        elif bps >= 1024:
+            Kbps = bps / 1024
+            msg = "{0}%   {1:.2f} Kbps".format(int(percent * 100), Kbps)
+        else:
+            msg = "{0}%   {1:.2f} bps".format(int(percent * 100), bps)
+        return msg
 
     def queue_event(self, event_type, event_text=None):
         """ Adds an event to Cnchi event queue """
