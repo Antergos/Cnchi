e0a79e4faf175e8c9403128c8f78bfa9790474c0
karasu <gustaucastells@gmail.com>
2015-12-05 12:44:13 +0100

    pep8 bootloader

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.13.295"
+CNCHI_VERSION = "0.13.296"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "development"
 
