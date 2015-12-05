5abdf19b44f25de04d8d5164897b03cc4b99fce4
karasu <gustaucastells@gmail.com>
2015-12-05 14:38:33 +0100

    pep8 lamp.py

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.13.299"
+CNCHI_VERSION = "0.13.300"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "development"
 
