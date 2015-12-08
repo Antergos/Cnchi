f8c0309e0df5ffbf50a107b117464120d4d93735
karasu <gustaucastells@gmail.com>
2015-12-08 13:30:12 +0100

    pep8 on keyboard_widget and keyboard_names

--- cnchi/misc/keyboard_names.py
+++ cnchi/misc/keyboard_names.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 #
-#  Copyright (C) 2012 Canonical Ltd.
-#  Written by Colin Watson <cjwatson@ubuntu.com>.
-#  Copyright © 2013-2015 Antergos
+# keyboard_names.py
 #
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  (at your option) any later version.
+# Copyright © 2013-2015 Antergos
 #
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
 #
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 """ Parse base.xml """
 
