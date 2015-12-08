c46334416fc0adf6fccb1b1369683c2e5421a7fb
karasu <gustaucastells@gmail.com>
2015-12-08 14:42:15 +0100

    pep8 pacman.conf

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.13.314"
+CNCHI_VERSION = "0.13.315"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "development"
 
