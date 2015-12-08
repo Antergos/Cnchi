c64fbd359eb0857e7e01b0ee82f89033e105765d
karasu <gustaucastells@gmail.com>
2015-12-08 11:58:03 +0100

    Pep8 (and bugfixing) zfs.py

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.13.303"
+CNCHI_VERSION = "0.13.304"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "development"
 
