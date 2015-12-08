dde4bae73cb5ee08bcd7b068e9661feacec3d023
karasu <gustaucastells@gmail.com>
2015-12-08 09:21:46 +0100

    pep8 on systemd_networkd

--- cnchi/installation/systemd_networkd.py
+++ cnchi/installation/systemd_networkd.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  systemd_networkd.py
+# systemd_networkd.py
 #
-#  Copyright © 2013-2015 Antergos
+# Copyright © 2013-2015 Antergos
 #
-#  This file is part of Cnchi.
+# This file is part of Cnchi.
 #
-#  Cnchi is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  (at your option) any later version.
+# Cnchi is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
 #
-#  Cnchi is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+# Cnchi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#  The following additional terms are in effect as per Section 7 of the license:
+# The following additional terms are in effect as per Section 7 of the license:
 #
-#  The preservation of all legal notices and author attributions in
-#  the material or in the Appropriate Legal Notices displayed
-#  by works containing it is required.
+# The preservation of all legal notices and author attributions in
+# the material or in the Appropriate Legal Notices displayed
+# by works containing it is required.
 #
-#  You should have received a copy of the GNU General Public License
-#  along with Cnchi; If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with Cnchi; If not, see <http://www.gnu.org/licenses/>.
 
 
 """ systemd-networkd configuration in base install. """
@@ -43,6 +43,7 @@ DEST_DIR = "/install"
 
 
 def chroot_run(cmd):
+    """ Helper function """
     chroot.run(cmd, DEST_DIR)
 
 
@@ -83,7 +84,10 @@ def setup(ssid=None, passphrase=None):
         logging.warning("systemd-networkd configuration failed: %s", process_error)
         return
 
-    logging.debug("Found [%s] links and [%s] are wireless", " ".join(links), " ".join(links_wireless))
+    logging.debug(
+        "Found [%s] links and [%s] are wireless",
+        " ".join(links),
+        " ".join(links_wireless))
 
     # Setup DHCP by default for all interfaces found
     for link in links:
@@ -123,6 +127,7 @@ def setup(ssid=None, passphrase=None):
 
 
 if __name__ == '__main__':
-    def _(x): return x
-    DEST_DIR="/"
+    def _(msg):
+        return msg
+    DEST_DIR = "/"
     setup()
