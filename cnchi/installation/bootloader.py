194189b3fcfde78c4fd831328bbd94416e765fd6
karasu <gustaucastells@gmail.com>
2015-12-10 20:44:33 +0100

    Fix indentation error in bootloader

--- cnchi/installation/bootloader.py
+++ cnchi/installation/bootloader.py
@@ -605,8 +605,10 @@ class Bootloader(object):
             logging.info("Systemd-boot install completed successfully")
             self.settings.set('bootloader_installation_successful', True)
         except subprocess.CalledProcessError as process_error:
-            logging.error('Command %s failed. Error output: %s', process_error.cmd),
-                          process_error.output)
+            logging.error(
+                'Command %s failed. Error output: %s',
+                process_error.cmd,
+                process_error.output)
             self.settings.set('bootloader_installation_successful', False)
         except subprocess.TimeoutExpired:
             logging.error('Command %s timed out.', " ".join(cmd))
