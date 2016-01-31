e5e25278ca7422f3fbf6bdab1645aa223d07fd6c
karasu <gustaucastells@gmail.com>
2016-01-31 16:19:50 +0100

    First stable version

--- cnchi/info.py
+++ cnchi/info.py
@@ -3,7 +3,7 @@
 #
 #  info.py
 #
-#  Copyright © 2013-2015 Antergos
+#  Copyright © 2013-2016 Antergos
 #
 #  This file is part of Cnchi.
 #
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.13.470"
+CNCHI_VERSION = "0.14.1"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "development"
 
