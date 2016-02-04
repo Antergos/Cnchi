4ebb0656d8c4b904bd81fa2e44a4ca23eb90f187
Dustin Falgout <dustin@falgout.us>
2016-02-04 03:10:41 -0600

    fix file permissions (cherry picked from commit d211fa8)

--- utils/fix-permissions.sh
+++ utils/fix-permissions.sh
@@ -4,8 +4,13 @@ find "${PWD}" -print0 | while read -d $'\0' file
 do
 	owner=$(stat --format '%U' "${file}")
 	echo "${owner}" > /dev/null
-	if [[ "${owner}" = root ]]; then
+	if [[ "${owner}" = 'root' ]]; then
+		mode=$(stat --format '%a' "${file}")
+		echo "${mode}" > /dev/null
 		sudo chown 1000:100 "${file}";
-		git add "${file}"
+		sudo -u 1000 chmod "${mode}" "${file}"
+		if ! [[ "${file}" =~ .*\.git\/.* ]]; then
+			git add "${file}"
+		fi
 	fi
 done
