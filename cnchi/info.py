c959107989d743679d51d9e88718399feb2dff80
Dustin Falgout <dustin@falgout.us>
2016-07-02 22:13:36 -0500

    Fix code comment.

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.14.110"
+CNCHI_VERSION = "0.14.111"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "production"
 
