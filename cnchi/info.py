270a483d73495ed9d19ea632f3135f4cbb671335
karasu <karasu@antergos.com>
2016-07-12 18:48:09 +0200

    Added script to run Cnchi. I didn't want to modify "run" one. This one works for me

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.15.119"
+CNCHI_VERSION = "0.15.120"
 CNCHI_WEBSITE = "https://www.antergos.com"
 CNCHI_RELEASE_STAGE = "development"
 
