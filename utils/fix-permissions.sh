d986c4896102ef0b0e1622bed8afa42cf905e144
Dustin Falgout <dustin@falgout.us>
2016-07-24 14:33:34 -0500

    housekeeping

--- utils/fix-permissions.sh
+++ utils/fix-permissions.sh
@@ -2,17 +2,17 @@
 
 echo "${PWD}"
 
-find "${PWD}" ! -iname '*.pyc' -print0 | while read -d $'\0' file
+find "${PWD}" ! -iname '*.pyc' ! -path '**/__pycache__/**' ! -path '**/.git/**' -print0 | while read -d $'\0' _file
 do
-	owner=$(stat --format '%U' "${file}")
+	owner=$(stat --format '%U' "${_file}")
 	echo "${owner}" > /dev/null
 	if [[ "${owner}" = 'root' ]]; then
-		mode=$(stat --format '%a' "${file}")
+		mode=$(stat --format '%a' "${_file}")
 		echo "${mode}" > /dev/null
-		sudo chown 1000:100 "${file}";
-		sudo -u 1000 chmod "${mode}" "${file}"
-		if ! [[ "${file}" =~ .*\.git\/.* ]]; then
-			git add "${file}"
+		sudo chown 1000:100 "${_file}"
+		sudo -u 1000 chmod "${mode}" "${_file}"
+		if ! [[ "${_file}" =~ .*\.git\/.* ]]; then
+			git add "${_file}"
 		fi
 	fi
 done
