41adc3a0472e15305036b475178d35e3864b4c6c
Dustin Falgout <dustin@falgout.us>
2016-08-23 05:11:32 -0500

    fix issues with MATE configuration. renamed some images. (cherry picked from commit 080b6d7)

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.14.145"
+CNCHI_VERSION = "0.14.147"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "production"
 
