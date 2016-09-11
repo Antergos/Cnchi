288b1dd42650224528108f3bebb8bcfc3b5db1db
Dustin Falgout <dustin@falgout.us>
2016-09-10 19:24:57 -0500

    dont check permissions inside node-modules dir (its unnecessary and it takes forever)

--- utils/sh/fix-permissions.sh
+++ utils/sh/fix-permissions.sh
@@ -2,7 +2,12 @@
 
 echo "${PWD}"
 
-find "${PWD}" ! -iname '*.pyc' ! -path '**/__pycache__/**' ! -path '**/.git/**' -print0 | while read -d $'\0' _file
+find "${PWD}" \
+	! -iname '*.pyc' \
+	! -path '**/__pycache__/**' \
+	! -path '**/.git/**' \
+	! -path '**/node_modules/**' \
+	-print0 | while read -d $'\0' _file
 do
 	owner=$(stat --format '%U' "${_file}")
 	echo "${owner}" > /dev/null
