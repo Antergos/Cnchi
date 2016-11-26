87a422ff8a56cf4a5786f9241a9999814291119c
Dustin Falgout <dustin@falgout.us>
2016-11-25 21:13:31 -0600

    update pre-commit hook

--- utils/sh/fix-permissions.sh
+++ utils/sh/fix-permissions.sh
@@ -2,13 +2,19 @@
 
 echo "${PWD}"
 
-find "${PWD}" ! -iname '*.pyc' ! -path '**/__pycache__/**' ! -path '**/.git/**' -print0 | while read -d $'\0' _file
+{ find "${PWD}" \
+	! -iname '*.pyc' \
+	! -path '**/__pycache__/**' \
+	! -path '**/.git/**' \
+	! -path '**/node_modules/**' \
+	-print0
+} | while read -d $'\0' _file
 do
 	owner=$(stat --format '%U' "${_file}")
-	echo "${owner}" > /dev/null
+
 	if [[ "${owner}" = 'root' ]]; then
 		mode=$(stat --format '%a' "${_file}")
-		echo "${mode}" > /dev/null
+
 		sudo chown 1000:100 "${_file}"
 
 		if [[ '1000' = "$(id -u)" ]]; then
