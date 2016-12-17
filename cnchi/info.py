250ed1cb490ff961f0ebd713a9a9d2305eec72af
karasu <karasu@antergos.com>
2016-12-17 19:46:55 +0100

    Added packages to KDE and Openbox lists

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.14.209"
+CNCHI_VERSION = "0.14.210"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "production"
 
