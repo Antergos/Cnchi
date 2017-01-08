bc157b7425f246d3f054a4b299ef42edc4c0607d
Phil Wyett <philwyett.rebellion@gmail.com>
2017-01-08 15:19:24 -0600

    Update current 'linux-lts' version (#688)
    * Formatted commit data below...
    
    * Update current 'linux-lts' version.
    
    * Formatted commit data below...
    
    * Remove line that refers to a specific lts version.
    
    (cherry picked from commit ce947a1)

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.14.218"
+CNCHI_VERSION = "0.14.219"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "production"
 
