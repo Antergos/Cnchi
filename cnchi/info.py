20d41f4dd45e04abce0aa5866d5729b410a2c099
karasu <karasu@antergos.com>
2017-07-04 10:17:47 +0200

    Remove unused deepin packages (experimental)

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.15.281"
+CNCHI_VERSION = "0.15.282"
 CNCHI_WEBSITE = "https://www.antergos.com"
 CNCHI_RELEASE_STAGE = "development"
 
