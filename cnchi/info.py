97b8e7c412aac81d8c924108a8e0bb3bbd37177b
karasu <karasu@antergos.com>
2017-07-06 23:26:38 +0200

    Removed package

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.14.295"
+CNCHI_VERSION = "0.14.296"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "production"
 
