f0d3243f7e1e772a4704f8b66c39c2e21025181c
karasu <karasu@antergos.com>
2017-07-22 18:26:17 +0200

    Update packages.xml (deepin)

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.15.290"
+CNCHI_VERSION = "0.15.291"
 CNCHI_WEBSITE = "https://www.antergos.com"
 CNCHI_RELEASE_STAGE = "development"
 
