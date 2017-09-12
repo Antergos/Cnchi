1b4c811f8d5ed61740f7bca402a323f014ce3193
karasu <gustaucastells@gmail.com>
2017-09-12 23:27:28 +0200

    Do not install qt5ct in deepin

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.14.342"
+CNCHI_VERSION = "0.14.343"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "production"
 
