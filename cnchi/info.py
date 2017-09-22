255ad99c3c479cf4beccc654a7b7250001acf1bb
karasu <gustaucastells@gmail.com>
2017-09-23 00:27:50 +0200

    Add antergos-welcome package

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.14.343"
+CNCHI_VERSION = "0.14.344"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "production"
 
