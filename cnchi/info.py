2b2b430c36916da3e07538885bd50cc5b195e517
karasu <gustaucastells@gmail.com>
2017-09-23 13:23:58 +0200

    beautify packages.xml

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.14.355"
+CNCHI_VERSION = "0.14.356"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "production"
 
