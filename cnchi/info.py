9136f2ed7569cd7b65463a64e37c92bad79f43d2
karasu <gustaucastells@gmail.com>
2017-09-26 19:01:46 +0200

    Update packages.xml

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.15.306"
+CNCHI_VERSION = "0.15.307"
 CNCHI_WEBSITE = "https://www.antergos.com"
 CNCHI_RELEASE_STAGE = "development"
 
