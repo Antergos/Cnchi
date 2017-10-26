bc9ac348cb3d1a2c210093d24907d681be4c4355
karasu <gustaucastells@gmail.com>
2017-10-26 17:11:29 +0200

    Version

--- cnchi/info.py
+++ cnchi/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.14.380"
+CNCHI_VERSION = "0.14.382"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "production"
 
