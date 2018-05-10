662dc61eb53e8a7e048fb13ceb6b996e250d5e17
karasu <karasu@antergos.com>
2018-05-10 08:50:58 +0200

    Add user icon

--- src/info.py
+++ src/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.14.464"
+CNCHI_VERSION = "0.14.465"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "production"
 
