7aeaed481ec55c0f42ef0c430bb025b94efd4e72
karasu <karasu@antergos.com>
2018-05-25 11:00:02 +0200

    Working on it. We need a list of partitions, not devices

--- src/pages/cache.py
+++ src/pages/cache.py
@@ -27,7 +27,7 @@
 # along with Cnchi; If not, see <http://www.gnu.org/licenses/>.
 
 
-""" Cache selection screen (optional) """
+""" Cache selection screen """
 
 import os
 import logging
@@ -41,21 +41,20 @@ import parted
 from pages.gtkbasebox import GtkBaseBox
 
 import misc.extra as misc
+from misc.run_cmd import call
 import parted3.fs_module as fs
-
-DEST_DIR = "/install"
 
 
 class Cache(GtkBaseBox):
-    """ Cache selection screen (optional) """
+    """ Cache selection screen"""
 
     def __init__(self, params, prev_page="installation_ask", next_page="summary"):
         super().__init__(self, params, "cache", prev_page, next_page)
 
-        self.cache_device = None
-        self.device_store = self.ui.get_object('select_drive')
-        self.device_label = self.ui.get_object('select_drive_label')
-        self.devices = {}
+        self.cache_partition = None
+        self.part_store = self.ui.get_object('select_part')
+        self.part_label = self.ui.get_object('select_part_label')
+        self.partitions = {}
 
     def translate_ui(self):
         """ Translate widgets """
@@ -79,16 +78,18 @@ class Cache(GtkBaseBox):
 
         self.header.set_subtitle(_("Cache selection (optional)"))
 
-    def populate_devices(self):
+    def populate_partitions(self):
         """ Fill list with devices """
         with misc.raised_privileges() as __:
             device_list = parted.getAllDevices()
 
-        self.device_store.remove_all()
-        self.devices = {}
+        self.part_store.remove_all()
+        self.partitions = {}
 
-        self.device_store.append_text("None")
-        self.devices["None"] = None
+        self.part_store.append_text("None")
+        self.partitions["None"] = None
+
+        devices = {}
 
         for dev in device_list:
             # avoid cdrom and any raid, lvm volumes or encryptfs
@@ -98,8 +99,8 @@ class Cache(GtkBaseBox):
                 size_in_gigabytes = int(
                     (dev.length * dev.sectorSize) / 1000000000)
                 line = '{0} [{1} GB] ({2})'.format(dev.model, size_in_gigabytes, dev.path)
-                self.device_store.append_text(line)
-                self.devices[line] = dev.path
+                #self.device_store.append_text(line)
+                devices[line] = dev.path
                 logging.debug(line)
 
         self.select_first_combobox_item(self.device_store)
@@ -111,7 +112,7 @@ class Cache(GtkBaseBox):
         tree_iter = tree_model.get_iter_first()
         combobox.set_active_iter(tree_iter)
 
-    def on_select_drive_changed(self, _widget):
+    def select_part_changed(self, _widget):
         """ User selected another drive """
         line = self.device_store.get_active_text()
         if line is not None:
@@ -122,16 +123,18 @@ class Cache(GtkBaseBox):
     def prepare(self, direction):
         """ Get screen ready """
         self.translate_ui()
-        self.populate_devices()
+        self.populate_partitions()
         self.show_all()
 
     def mount_device(self):
         """ Mounts cache device into a temporary folder """
-        device = self.cache_device
+        device = self.cache_partition
         if device:
-            tmp_dir = tempfile.mkdtemp()
-            call(["mount", device, tmp_dir])
-            return tmp_dir
+            mount_dir = ("/mnt/cnchi-cache")
+            if not os.path.exists(mount_dir):
+                os.makedirs(mount_dir)
+            call(["mount", device, mount_dir])
+            return mount_dir
         else:
             return None
 
@@ -140,8 +143,9 @@ class Cache(GtkBaseBox):
         
         xz_cache_dir = self.mount_device()
         if xz_cache_dir:
-            xz_cache = self.settings.get('xz_cache', xz_cache)
+            xz_cache = self.settings.get('xz_cache')
             xz_cache.append(xz_cache_dir)
+            self.settings.set('xz_cache', xz_cache)
         return True
 
 # When testing, no _() is available
