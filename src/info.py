59b69a409bc7302eaa5bb69999b925f6e1b44e25
karasu <gustaucastells@gmail.com>
2018-06-28 20:01:13 +0200

    Remove trailing whitespaces

--- src/info.py
+++ src/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.16.130"
+CNCHI_VERSION = "0.16.131"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "production"
 
