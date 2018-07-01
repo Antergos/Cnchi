0f9e1ee9a0b63d805685682346d3406ce78f1919
karasu <gustaucastells@gmail.com>
2018-07-01 16:43:31 +0200

    It doesn't work anyways

--- src/info.py
+++ src/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.16.166"
+CNCHI_VERSION = "0.16.167"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "production"
 
