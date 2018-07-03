82a9027e03d617d274b186367cf31fa64c845805
karasu <gustaucastells@gmail.com>
2018-07-03 20:04:08 +0200

    Change requirements.txt filename

--- src/info.py
+++ src/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.16.177"
+CNCHI_VERSION = "0.16.178"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "production"
 
