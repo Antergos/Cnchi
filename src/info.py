bf4606a6cd4dd027f280948bcbe9670bd96b6258
karasu <gustaucastells@gmail.com>
2018-07-06 19:05:21 +0200

    Trying to run mkdocs

--- src/info.py
+++ src/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.16.181"
+CNCHI_VERSION = "0.16.182"
 CNCHI_WEBSITE = "http://www.antergos.com"
 CNCHI_RELEASE_STAGE = "production"
 
