2ea9c20ddf36154af9da7f6f84c897697f57d7d3
karasu <gustaucastells@gmail.com>
2018-08-30 16:43:25 +0200

    python-urllib3 is needed

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -9,6 +9,6 @@ sudo pacman -S iso-codes gptfdisk python python-bugsnag \
 python-cairo python-chardet python-dbus python-defusedxml \
 python-feedparser python-geoip2 python-gobject python-idna \
 python-libnacl python-mako python-maxminddb python-parted \
-python-requests upower
+python-requests upower python-urllib3
 
 
--- src/info.py
+++ src/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "0.15.289"
+CNCHI_VERSION = "0.15.290"
 """ Cnchi version """
 
 CNCHI_WEBSITE = "http://www.antergos.com"
