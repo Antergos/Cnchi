c31abd22d831737f67b762878084e408f6d46bb4
karasu <gustaucastells@gmail.com>
2018-08-30 15:47:09 +0200

    Update requirements script

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-sudo pacman -Syy
-sudo pacman -S iso-codes geoip2-database gptfdisk python pyalpm python-bugsnag \
+sudo pacman -Syy --noconfirm
+sudo pacman -Scc --noconfirm
+sudo pacman -S geoip2-database
+sudo pacman -Scc --noconfirm
+sudo pacman -S python
+sudo pacman -Scc --noconfirm
+sudo pacman -S webkit2gtk
+sudo pacman -Scc --noconfirm
+sudo pacman -S iso-codes gptfdisk python python-bugsnag \
 python-cairo python-chardet python-dbus python-defusedxml \
 python-feedparser python-geoip2 python-gobject python-idna \
 python-libnacl python-mako python-maxminddb python-parted \
-python-requests upower webkit2gtk
+python-requests upower
+sudo pacman -Scc --noconfirm
+
 
