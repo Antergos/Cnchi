d0a290a7642d32646fde378d29f20923495ca4ff
karasu <gustaucastells@gmail.com>
2018-08-30 15:43:54 +0200

    Added small script to install all requirements

---
+++ scripts/requirements.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo pacman -Syy
+sudo pacman -S iso-codes geoip2-database gptfdisk python pyalpm python-bugsnag \
+python-cairo python-chardet python-dbus python-defusedxml \
+python-feedparser python-geoip2 python-gobject python-idna \
+python-libnacl python-mako python-maxminddb python-parted \
+python-requests upower webkit2gtk
+
