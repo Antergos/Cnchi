8fe8046620f5784a8d631d321607cae229805df7
karasu <gustaucastells@gmail.com>
2018-08-30 15:59:08 +0200

    Updated script requirements.sh

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
+#cow_spacesize=1G
+sudo mount -o remount,size=1G /run/archiso/cowspace
 sudo pacman -Syy --noconfirm
-sudo pacman -Scc --noconfirm
 sudo pacman -S geoip2-database
-sudo pacman -Scc --noconfirm
 sudo pacman -S python
-sudo pacman -Scc --noconfirm
 sudo pacman -S webkit2gtk
-sudo pacman -Scc --noconfirm
 sudo pacman -S iso-codes gptfdisk python python-bugsnag \
 python-cairo python-chardet python-dbus python-defusedxml \
 python-feedparser python-geoip2 python-gobject python-idna \
 python-libnacl python-mako python-maxminddb python-parted \
 python-requests upower
-sudo pacman -Scc --noconfirm
 
 
